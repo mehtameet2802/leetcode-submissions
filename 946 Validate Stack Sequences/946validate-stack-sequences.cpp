cc6ce404b3d45c0d6d6bed7e3cae0736e6b09db5
Meet Mehta <mehtameetoff2802@gmail.com>
2023-03-02 15:36:19 +0000

    Code challenge submission for question: 946 Validate Stack Sequences

---
+++ 946 Validate Stack Sequences/946validate-stack-sequences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int i=0;
+        int j=0;
+        while(i<pushed.size()){
+            st.push(pushed[i]);
+            while(!st.empty() && st.top() == popped[j]){
+                st.pop();
+                j++;
+            }
+            i++;
+        }
+        if(st.size()>0)
+            return false;
+        return true;
+    }
+};

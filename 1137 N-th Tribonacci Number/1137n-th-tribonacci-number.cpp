22ef4699b789d2802d0815303c0c6e50c1309717
Meet Mehta <mehtameetoff2802@gmail.com>
2023-04-09 09:56:29 +0000

    Code challenge submission for question: 1137 N-th Tribonacci Number

---
+++ 1137 N-th Tribonacci Number/1137n-th-tribonacci-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int x0 = 0;
+        int x1 = 1;
+        int x2 = 1;
+
+        if(n==0)
+            return 0;
+        
+        if(n==1||n==2)
+            return 1;
+
+        for(int i=3;i<=n;i++){
+            int cur = x0+x1+x2;
+            x0 = x1;
+            x1 = x2;
+            x2 = cur;
+        }
+
+        return x2;
+    }
+};

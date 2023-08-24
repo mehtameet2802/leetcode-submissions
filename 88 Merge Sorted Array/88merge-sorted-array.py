cd857fb0fb6ad642cd1c16e73460222e72042205
Meet Mehta <mehtameetoff2802@gmail.com>
2023-08-24 12:08:51 +0000

    Code challenge submission for question: 88 Merge Sorted Array

---
+++ 88 Merge Sorted Array/88merge-sorted-array.py
@@ -0,0 +1,19 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if n==0:
+            return
+        
+        if m==0:
+            for i in range(n):
+                nums1[i] = nums2[i]
+            return
+        
+        for i in range(n):
+            nums1[m+i] = nums2[i]
+        
+        nums1.sort()
+        
+            

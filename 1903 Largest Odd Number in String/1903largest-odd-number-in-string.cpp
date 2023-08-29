d66c093a78fd01f9d98e14b7eb1650548b45f8c8
Meet Mehta <mehtameetoff2802@gmail.com>
2023-08-29 16:09:24 +0000

    Code challenge submission for question: 1903 Largest Odd Number in String

---
+++ 1903 Largest Odd Number in String/1903largest-odd-number-in-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int ans = 0;
+    string largestOddNumber(string num) {
+        for(int i=num.size()-1;i>=0;i--){
+            if(num[i]%2!=0){
+                return num.substr(0,i+1);
+            }
+        }
+        return "";
+    }
+};

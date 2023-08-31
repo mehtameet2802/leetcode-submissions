a121490a0ccee65a3d2403b97a3425d22a9136ab
Meet Mehta <mehtameetoff2802@gmail.com>
2023-08-31 04:51:04 +0000

    Code challenge submission for question: 5 Longest Palindromic Substring

---
+++ 5 Longest Palindromic Substring/5longest-palindromic-substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string ans = "";
+        for(int i=0;i<s.size();i++){
+            int l = i;
+            int r = i;
+            while(l>=0 && r<s.size() && s[l]==s[r]){
+                if(r-l+1>ans.length()){
+                    ans = s.substr(l,r-l+1);
+                }
+                l--;
+                r++;
+            }
+
+            l = i;
+            r = i+1;
+            while(l>=0 && r<s.size() && s[l]==s[r]){
+                if(r-l+1>ans.length()){
+                    ans = s.substr(l,r-l+1);
+                }
+                l--;
+                r++;
+            }
+
+        }
+        return ans;
+    }
+};

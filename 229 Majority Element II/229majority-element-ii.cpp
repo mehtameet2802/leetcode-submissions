96c6fda4be0360d1ac9545ec826d6111ccb81708
Meet Mehta <mehtameetoff2802@gmail.com>
2024-07-03 08:09:58 +0000

    Code challenge submission for question: 229 Majority Element II

---
+++ 229 Majority Element II/229majority-element-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        vector<int> ans;
+        int ele1 =INT_MIN;
+        int ele2 = INT_MIN;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(int i=0;i<nums.size();i++){
+            if(cnt1==0 && ele2!=nums[i]){
+                cnt1 = 1;
+                ele1=nums[i];
+            }
+            else if(cnt2==0 && ele1!=nums[i]){
+                cnt2=1;
+                ele2=nums[i];
+            }
+            else if(ele1==nums[i])
+                cnt1++;
+            else if(ele2==nums[i])
+                cnt2++;
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==ele1)
+                cnt1++;
+            else if(nums[i]==ele2)
+                cnt2++;
+        }
+
+        int x = nums.size()/3;
+        if(cnt1>x)
+            ans.push_back(ele1);
+        if(cnt2>x)
+            ans.push_back(ele2);
+        return ans;
+    }
+};

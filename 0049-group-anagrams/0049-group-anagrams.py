f3cc69d427e17d8812d1b2b9298e2282eb705df0
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-07-29 18:36:51 +0530

    Time: 19 ms (19.66%), Space: 23.9 MB (16.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,45 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
+        # """
+        # Pattern - Use space to reduce time
+
+        # TC - O(m.nlogn)
+        # SC - O(mn)
+        # """
+
+
+        # k_map = defaultdict(list)
+
+        # for word in strs:
+        #     key = "".join(sorted(word))
+        #     k_map[key].append(word)
+        
+        # ans = []
+        # for key, item in k_map.items():
+        #     ans.append(item)
+        
+        # return ans
+
+
+
+        """
+        Pattern - Use space to reduce time
+
+        TC - O(m.n)
+        SC - O(mn)
+        """
+
 
         k_map = defaultdict(list)
 
         for word in strs:
-            key = "".join(sorted(word))
-            k_map[key].append(word)
+            key = [0] * 26
+
+            for ch in word:
+                key[ord(ch) - ord('a')]+=1
+            
+            k_map[tuple(key)].append(word)
         
         ans = []
         for key, item in k_map.items():

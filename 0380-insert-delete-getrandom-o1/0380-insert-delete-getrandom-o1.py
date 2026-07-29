a282a5499ef3c958be44614614b4e460ca022f5e
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-07-29 17:59:29 +0530

    Time: 150 ms (23.12%), Space: 56.8 MB (89.23%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.py
@@ -2,27 +2,75 @@ import random
 
 class RandomizedSet:
 
+    # """
+    # Pattern - Use space to reduce time
+
+    # TC - O(1) - for first 2, O(n) - for last 1
+    # SC - O(n)
+
+    # """
+
+
+    # def __init__(self):
+    #     self.seen = set()
+
+    # def insert(self, val: int) -> bool:
+    #     if val in self.seen:
+    #         return False
+        
+    #     self.seen.add(val)
+    #     return True
+        
+
+    # def remove(self, val: int) -> bool:
+    #     if val not in self.seen:
+    #         return False
+        
+    #     self.seen.remove(val)
+    #     return True
+        
+
+    # def getRandom(self) -> int:
+    #     return random.choice(list(self.seen))
+
+
+    
+    # Optimized
+    """
+    Pattern - Use space to reduce time
+
+    TC - O(1) - for first all 3
+    SC - O(n)
+
+    """
+
+
     def __init__(self):
-        self.seen = set()
+        self.arr = []
+        self.e_map = {}
 
     def insert(self, val: int) -> bool:
-        if val in self.seen:
+        if val in self.e_map:
             return False
         
-        self.seen.add(val)
+        self.arr.append(val)
+        self.e_map[val] = len(self.arr)-1
         return True
         
 
     def remove(self, val: int) -> bool:
-        if val not in self.seen:
+        if val not in self.e_map:
             return False
         
-        self.seen.remove(val)
+        ind = self.e_map[val]
+        self.e_map.pop(val)
+        self.arr.remove(val)
         return True
         
 
     def getRandom(self) -> int:
-        return random.choice(list(self.seen))
+        return random.choice(self.arr)
+
 
 
 # Your RandomizedSet object will be instantiated and called as such:

3678d5a1579da20335181bc9edebcd0fb8518766
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-07-30 13:59:33 +0530

    Time: 3 ms (59.13%), Space: 21.3 MB (23.34%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,7 +1,47 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
+        '''
+        Pattern - Recursion + DP
+
+        TC - O(n)
+        SC - O(n)
+        '''
+
         mem = {}
+
+        n = len(prices)
+
+        profit = float('-inf')
+
+        def helper(i, have_stock):
+
+            if (i,have_stock) in mem:
+                return mem[(i,have_stock)]
+
+            if i>=n:
+                return 0
+            
+            if have_stock:
+                a1 = helper(i+2, False) + prices[i]
+            else:
+                a1 = helper(i+1, True) - prices[i]
+            
+            a2 = helper(i+1, have_stock)
+            ans = max(a1,a2)
+
+            mem[(i,have_stock)] = ans
+            return ans
+
+        return helper(0, False)
+
+
+        '''
+        Pattern - Recursion + DP
+
+        TC - O(n)
+        SC - O(1)
+        '''
 
         n = len(prices)
 
@@ -27,4 +67,3 @@ class Solution:
             return ans
 
         return helper(0, False, 0)
-        return profit

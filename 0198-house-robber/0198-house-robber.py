711ad5084fa64e2605cc00407e2b7503e7143ddc
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-07-30 12:34:43 +0530

    Time: 0 ms (100%), Space: 19.2 MB (89.83%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,23 +1,44 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # '''
+        # Pattern - Recursion
+
+        # TC - O(n)
+        # SC - O(n)
+        # '''
+
+        # mem = {}
+
+        # def helper(i):
+        #     if i in mem:
+        #         return mem[i]
+
+        #     if i>=len(nums):
+        #         return 0
+            
+        #     ans = max(helper(i+2)+nums[i],helper(i+1))
+        #     mem[i] = ans
+        #     return ans
+        
+        # return helper(0)
+
+
         '''
         Pattern - Recursion
 
         TC - O(n)
-        SC - O(n)
+        SC - O(1)
         '''
 
-        mem = {}
-
-        def helper(i):
-            if i in mem:
-                return mem[i]
-
-            if i>=len(nums):
-                return 0
-            
-            ans = max(helper(i+2)+nums[i],helper(i+1))
-            mem[i] = ans
-            return ans
+        n = len(nums)
+        if n<3:
+            return max(nums)
         
-        return helper(0)
+        last = nums[n-1]
+        prev1 = max(nums[n-1],nums[n-2])
+
+        for i in range(n-3,-1,-1):
+            cur = max(nums[i]+last,prev1)
+            last, prev1 = prev1, cur
+        
+        return max(last, prev1)

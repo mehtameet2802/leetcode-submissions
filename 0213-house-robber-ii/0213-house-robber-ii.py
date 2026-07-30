e851fbcc0c06a065d3261c91a877ce30a23d4665
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-07-30 13:16:45 +0530

    Time: 0 ms (100%), Space: 19.6 MB (10.61%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        '''
+        Pattern - 
+
+        TC - O(n)
+        SC - O(n)
+        '''
+
+        mem = {}
+
+        n = len(nums)
+
+        if n == 1:
+            return nums[0]
+        
+        def helper(i, first_taken):
+            if (i,first_taken) in mem:
+                return mem[(i,first_taken)]
+
+            if i>=n:
+                return 0
+            
+            if i == n-1:
+                if first_taken:
+                    return 0
+                return nums[i]
+            
+            ans = max(helper(i+2,first_taken)+nums[i], helper(i+1,first_taken))
+            mem[(i,first_taken)] = ans
+            return ans
+        
+        return max(helper(0,True), helper(1, False))

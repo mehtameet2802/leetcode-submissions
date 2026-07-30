ce5efb463fc03f577488b9444fae51c11cd9ce02
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-07-30 13:31:04 +0530

    Time: 0 ms (100%), Space: 19.2 MB (68.5%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,34 +1,60 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        # '''
+        # Pattern - 
+
+        # TC - O(n)
+        # SC - O(n)
+        # '''
+
+        # mem = {}
+
+        # n = len(nums)
+
+        # if n == 1:
+        #     return nums[0]
+        
+        # def helper(i, first_taken):
+        #     if (i,first_taken) in mem:
+        #         return mem[(i,first_taken)]
+
+        #     if i>=n:
+        #         return 0
+            
+        #     if i == n-1:
+        #         if first_taken:
+        #             return 0
+        #         return nums[i]
+            
+        #     ans = max(helper(i+2,first_taken)+nums[i], helper(i+1,first_taken))
+        #     mem[(i,first_taken)] = ans
+        #     return ans
+        
+        # return max(helper(0,True), helper(1, False))
+
+
         '''
         Pattern - 
 
         TC - O(n)
-        SC - O(n)
+        SC - O(1)
         '''
-
-        mem = {}
 
         n = len(nums)
 
         if n == 1:
             return nums[0]
         
-        def helper(i, first_taken):
-            if (i,first_taken) in mem:
-                return mem[(i,first_taken)]
+        def robber(s,e):
 
-            if i>=n:
-                return 0
+            last = nums[e]
+            prev1 = max(nums[e-1],last)
+
+            for i in range(e-2,s-1,-1):
+                cur = max(nums[i]+last,prev1)
+                last, prev1 = prev1, cur
             
-            if i == n-1:
-                if first_taken:
-                    return 0
-                return nums[i]
-            
-            ans = max(helper(i+2,first_taken)+nums[i], helper(i+1,first_taken))
-            mem[(i,first_taken)] = ans
-            return ans
+            return prev1
         
-        return max(helper(0,True), helper(1, False))
+        return max(robber(0,n-2), robber(1, n-1))

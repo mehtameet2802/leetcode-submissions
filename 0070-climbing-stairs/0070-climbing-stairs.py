88196dcb147fb1ceffd56e18293d0c2aabdfc5d3
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-07-30 12:11:44 +0530

    Time: 0 ms (100%), Space: 19.1 MB (86.23%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,20 +1,47 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        mem = {}
+        # '''
+        # Pattern - Recursion + DP
+
+        # TC - O(n)
+        # SC - O(n)
+        # '''
+
+        # mem = {}
         
-        def helper(i):
-            if i in mem:
-                return mem[i]
+        # def helper(i):
+        #     if i in mem:
+        #         return mem[i]
             
-            if i == n:
-                return 1
+        #     if i == n:
+        #         return 1
             
-            if i>n:
-                return 0
+        #     if i>n:
+        #         return 0
             
-            ans = helper(i+1) + helper(i+2)
-            mem[i] = ans
-            return ans
+        #     ans = helper(i+1) + helper(i+2)
+        #     mem[i] = ans
+        #     return ans
         
-        return helper(0)
+        # return helper(0)
+
+
+        '''
+        Pattern - Recursion + DP
+
+        TC - O(n)
+        SC - O(1)
+        '''
+
+        if n<=2:
+            return n
+
+        prev_1 = 1
+        prev_2 = 2
+        
+        for _ in range(n-3,-1,-1):
+            prev_3 = prev_1 + prev_2
+            prev_1, prev_2 = prev_2, prev_3
+
+        return prev_2

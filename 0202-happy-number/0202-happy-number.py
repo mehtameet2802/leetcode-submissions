a75f85834ff732f20690e4571797ca741a487810
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-07-30 14:44:30 +0530

    Time: 0 ms (100%), Space: 19.2 MB (89.96%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,29 +1,29 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
 
-        '''
-        TC - O(n*len(n))
-        SC - O(n)
-        '''
+        # '''
+        # TC - O(n*len(n))
+        # SC - O(n)
+        # '''
 
-        seen = set()
+        # seen = set()
 
-        def cal(num):
-            new_num = 0
-            while num>0:
-                new_num += pow(num%10,2)
-                num = num//10
-            return new_num
+        # def cal(num):
+        #     new_num = 0
+        #     while num>0:
+        #         new_num += pow(num%10,2)
+        #         num = num//10
+        #     return new_num
 
 
-        while n not in seen:
-            seen.add(n)
-            if n == 1:
-                return True
-            n = cal(n)
+        # while n not in seen:
+        #     seen.add(n)
+        #     if n == 1:
+        #         return True
+        #     n = cal(n)
             
         
-        return False
+        # return False
 
 
         '''
@@ -45,8 +45,8 @@ class Solution:
         fast = cal(n)
 
         while fast!=1 and slow!=fast:
-            fast = cal(cal(n))
-            slow = cal(n)
+            fast = cal(cal(fast))
+            slow = cal(slow)
 
         
         return fast == 1

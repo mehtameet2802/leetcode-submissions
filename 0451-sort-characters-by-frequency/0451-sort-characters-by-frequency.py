4620cc26ff955c2c47152ae947580240df766be0
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-01 13:12:32 +0530

    Time: 7 ms (79.46%), Space: 20 MB (88.79%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -3,15 +3,41 @@ from collections import Counter
 
 class Solution:
     def frequencySort(self, s: str) -> str:
-        max_heap = []
-        f_map = Counter(s)
 
-        for ch, f in f_map.items():
-            heapq.heappush(max_heap, (-f,ch))
+        # '''
+        # Pattern - Heaps
+
+        # N - len of string 
+        # TC - O(N log N)
+        # SC - O(N)
+        # '''
+
+        # max_heap = []
+        # f_map = Counter(s)
+
+        # for ch, f in f_map.items():
+        #     heapq.heappush(max_heap, (-f,ch))
         
-        ans = ""
-        while max_heap:
-            f, ch = heapq.heappop(max_heap)
-            ans += (ch*-f)
+        # ans = ""
+        # while max_heap:
+        #     f, ch = heapq.heappop(max_heap)
+        #     ans += (ch*-f)
             
-        return ans
+        # return ans
+
+
+        '''
+        Pattern - Heaps
+
+        N - len of string 
+        TC - O(N log N)
+        SC - O(N)
+        '''
+
+        f_map = Counter(s)
+        ans = []
+
+        for ch, f in sorted(f_map.items(), key = lambda x:x[1], reverse=True):
+            ans.append(ch*f)
+            
+        return "".join(ans)

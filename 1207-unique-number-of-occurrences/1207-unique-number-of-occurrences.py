c9ae6f314d7dba2cea2d8629aaa74d2084e73f17
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-01 12:26:19 +0530

    Time: 3 ms (17.29%), Space: 19.2 MB (76.42%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,8 +1,46 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
+
+        # '''
+        # Pattern - 
+
+        # TC - O(n)
+        # SC - O(1)
+        # '''
+
+        # f_arr = [[0,0] for _ in range(1001)]
+
+        # o_map = defaultdict(int)
+
+        # for num in arr:
+        #     if num < 0:
+        #         f_arr[-num][1] += 1
+        #     else:
+        #         f_arr[num][0] += 1
+        
+        # for a,b in f_arr:            
+        #     if a>0:
+        #         o_map[a] += 1
+            
+        #     if b>0:
+        #         o_map[b] += 1
+            
+        #     if o_map[a]>1 or o_map[b]>1:
+        #         return False
+        
+        # return True
+
+
+        '''
+        Pattern - 
+
+        TC - O(n)
+        SC - O(1)
+        '''
+
         f_arr = [[0,0] for _ in range(1001)]
 
-        o_map = defaultdict(int)
+        o_set = set()
 
         for num in arr:
             if num < 0:
@@ -12,14 +50,15 @@ class Solution:
         
         for a,b in f_arr:            
             if a>0:
-                o_map[a] += 1
+                if a in o_set:
+                    return False
+                o_set.add(a)
             
             if b>0:
-                o_map[b] += 1
-            
-            if o_map[a]>1 or o_map[b]>1:
-                return False
-        
+                if b in o_set:
+                    return False
+                o_set.add(b)
+                    
         return True
         
 

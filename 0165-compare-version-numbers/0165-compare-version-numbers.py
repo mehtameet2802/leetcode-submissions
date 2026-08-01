fa66c85c4ab172419f479228fda7ee1e47fde324
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-01 20:07:01 +0530

    Time: 0 ms (100%), Space: 19.3 MB (67.02%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -0,0 +1,20 @@
+class Solution:
+    def compareVersion(self, version1: str, version2: str) -> int:
+        v1 = version1.split(".")
+        v2 = version2.split(".")
+
+        m = max(len(v1),len(v2))
+
+        i = 0
+        while i<m:
+            a = int(v1[i]) if i < len(v1) else 0
+            b = int(v2[i]) if i < len(v2) else 0
+
+            if a > b:
+                return 1
+            elif a < b:
+                return -1
+            
+            i += 1
+
+        return 0

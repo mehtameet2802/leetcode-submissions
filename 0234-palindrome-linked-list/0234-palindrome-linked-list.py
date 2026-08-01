61cdeb2906d17a1070a49168fd1d1d9276e1c6da
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-01 21:04:24 +0530

    Time: 59 ms (11.97%), Space: 37.7 MB (99.06%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,10 +5,49 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        s1 = []
+        # s1 = []
 
-        while head:
-            s1.append(head.val)
-            head = head.next
+        # while head:
+        #     s1.append(head.val)
+        #     head = head.next
         
-        return s1 == s1[::-1]
+        # return s1 == s1[::-1]
+
+        if not head or not head.next:
+            return True
+
+        fast = head
+        slow = head
+
+        prev = None
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+
+        if fast:
+            head2 = slow.next
+            prev.next = None
+        else:
+            head2 = slow
+            prev.next = None
+
+        prev = None
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        
+        head = prev
+        while head and head2:
+            if head.val != head2.val:
+                return False
+            
+            head = head.next
+            head2 = head2.next
+        
+        if not head and not head2:
+            return True
+        
+        return False

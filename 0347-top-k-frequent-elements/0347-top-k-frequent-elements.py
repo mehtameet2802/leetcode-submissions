02afe6f1d524e293a5561c743a9056d6ff40f920
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-01 13:32:32 +0530

    Time: 0 ms (100%), Space: 22.9 MB (41.64%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,53 @@
+import heapq
 from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        f_map = Counter(nums)
+        # '''
+        # Pattern - Frequency Count
 
+        # N - len(nums)
+        # TC - O(N + M log M)
+        # SC - O(N)
+        # '''
+
+        # f_map = Counter(nums)
+
+        # ans = []
+
+        # for val, f in sorted(f_map.items(),key=lambda x:x[1], reverse=True):
+        #     if k>0:
+        #         ans.append(val)
+        #         k-=1
+        #     else:
+        #         break
+        
+        # return ans
+
+
+        '''
+        Pattern - Frequency Count
+
+        N - len(nums)
+        M - unique elements
+        TC - O(N + M log k)
+        SC - O(N)
+        '''
+
+        f_map = Counter(nums)
+        min_heap = []
         ans = []
 
-        for val, f in sorted(f_map.items(),key=lambda x:x[1], reverse=True):
-            if k>0:
-                ans.append(val)
-                k-=1
+        for val, f in f_map.items():
+            if min_heap and k==0: 
+                if f > min_heap[0][0]:
+                    heapq.heappop(min_heap)
+                    heapq.heappush(min_heap, (f,val))
             else:
-                break
-        
-        return ans
+                heapq.heappush(min_heap, (f,val))
+                k-=1
 
+        for item in min_heap:
+            ans.append(item[1])
+
+        return ans

1ca6adb62ccdab8328e51862880315330e13b2e3
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-02 11:37:21 +0530

    Time: 3 ms (35.03%), Space: 20.6 MB (17.83%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.py
+++ 0665-non-decreasing-array/0665-non-decreasing-array.py
@@ -1,27 +1,67 @@
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
+
+        # '''
+        # Pattern - Greedy
+
+        # TC - O(N)
+        # SC - O(1)
+        # '''
+
+        # n = len(nums)
+
+        # if n==1:
+        #     return True
+        
+        # cnt = 0
+        # for i in range(n-1):
+        #     if nums[i] > nums[i+1]:
+        #         cnt += 1
+        
+        # if cnt > 1:
+        #     return False
+        
+        # for i in range(n-1):
+        #     if nums[i] > nums[i+1]:
+        #         if cnt > 0:
+        #             if i > 0 and nums[i-1] > nums[i+1]:
+        #                 nums[i+1] = nums[i]
+        #             else:
+        #                 nums[i] = nums[i+1]
+        #             cnt -= 1
+        #         else:
+        #             return False
+        
+        # return True
+
+
+
+        '''
+        Pattern - Greedy
+
+        TC - O(N)
+        SC - O(1)
+        '''
+
         n = len(nums)
 
         if n==1:
             return True
         
-        cnt = 0
-        for i in range(n-1):
-            if nums[i] > nums[i+1]:
-                cnt += 1
-        
-        if cnt > 1:
-            return False
+        changed = False
         
         for i in range(n-1):
             if nums[i] > nums[i+1]:
-                if cnt > 0:
-                    if i > 0 and nums[i-1] > nums[i+1]:
-                        nums[i+1] = nums[i]
-                    else:
-                        nums[i] = nums[i+1]
-                    cnt -= 1
-                else:
+
+                if changed:
                     return False
+
+                changed = True
+
+                if i > 0 and nums[i-1] > nums[i+1]:
+                    nums[i+1] = nums[i]
+                else:
+                    nums[i] = nums[i+1]
         
         return True
+

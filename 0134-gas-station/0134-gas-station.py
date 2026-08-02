b53d534a4cbc26179e3434bfffe0457e58881af1
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-02 13:10:39 +0530

    Time: 16 ms (83.71%), Space: 26 MB (29.97%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+
+        '''
+        Pattern - Greedy
+
+        TC - O(N)
+        SC - O(1)
+        '''
+
+        if sum(gas) < sum(cost):
+            return -1
+        
+        start = 0
+        tank = 0
+        for i in range(len(gas)):
+            tank += gas[i]-cost[i]
+
+            if tank < 0:
+                start = i+1
+                tank = 0
+
+        return start

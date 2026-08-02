7681447f6a8e527e2ee66f93637348066241ed0c
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-02 17:59:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (43.4%) - LeetHub

--- 0781-rabbits-in-forest/0781-rabbits-in-forest.py
+++ 0781-rabbits-in-forest/0781-rabbits-in-forest.py
@@ -1,12 +1,22 @@
 from collections import Counter
+from math import ceil
 
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
+
+        '''
+        Pattern - Greedy + Frequency Map
+
+        TC - O(N)
+        SC - O(U)   # U = number of unique answers
+        '''
+
         f_map = Counter(answers)
 
         ans = 0
 
         for value, f in f_map.items():
-            ans += ceil(f/(value+1)) * (value+1)
+            group_n = ceil(f / (value + 1))
+            ans += group_n * (value + 1)
 
         return ans

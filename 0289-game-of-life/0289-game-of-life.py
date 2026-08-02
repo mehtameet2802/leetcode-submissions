1b82962e8e3269bdb23e9f0c30caf034ce1a6cee
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-02 19:40:37 +0530

    Time: 0 ms (100%), Space: 19.4 MB (47.47%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.py
+++ 0289-game-of-life/0289-game-of-life.py
@@ -8,7 +8,7 @@ class Solution:
         Pattern - Matrix Iteration - Directions
 
         TC - O(R*C)
-        SC - O(R*C)
+        SC - O(1)
         '''
 
         dead = []
@@ -17,6 +17,14 @@ class Solution:
         COLS = len(board[0])
 
         dirs = [[0,1],[0,-1],[1,0],[-1,0],[1,1],[-1,-1],[1,-1],[-1,1]]
+
+        # | Value | Meaning     |
+        # | ----- | ----------- |
+        # | 0     | dead → dead |
+        # | 1     | live → live |
+        # | 2     | live → dead |
+        # | 3     | dead → live |
+
 
         def helper(r,c):
             l = 0 
@@ -27,29 +35,26 @@ class Solution:
                 if nr<0 or nc<0 or nr>=ROWS or nc>=COLS:
                     continue
 
-                if board[nr][nc] == 1:
+                if board[nr][nc] == 1 or board[nr][nc] == 2:
                     l += 1
 
             if board[r][c]:
                 if l>3 or l<2:
-                    return 0
+                    return 2
                 elif l==2 or l==3:
                     return 1
             else:
                 if l == 3:
-                    return 1
+                    return 3
                 return 0     
             
         for i in range(ROWS):
             for j in range(COLS):
-                if helper(i,j):
-                    live.append((i,j))
-                else:
-                    dead.append((i,j))
+                board[i][j] = helper(i,j)
         
-
-        for a,b in live:
-            board[a][b] = 1
-        
-        for a,b in dead:
-            board[a][b] = 0
+        for i in range(ROWS):
+            for j in range(COLS):
+                if board[i][j] == 3:
+                    board[i][j] = 1
+                elif board[i][j] == 2:
+                    board[i][j] = 0 

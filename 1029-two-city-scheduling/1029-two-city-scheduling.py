b88ef35e5ff15d11f1bd59854cf016261e5b8c1e
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-02 14:02:13 +0530

    Time: 0 ms (100%), Space: 19.4 MB (14.25%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -9,20 +9,15 @@ class Solution:
         '''
         
         diff = [0]*len(costs)
-
-        for i, ele in enumerate(costs):
-            a = ele[0]
-            b = ele[1]
-            costs[i] = (ele[0]-ele[1],a,b)
         
-        costs.sort(key=lambda x:x[0])
+        costs.sort(key=lambda x:x[0]-x[1])
 
         cost = 0
         n = len(costs)//2
         for i, ele in enumerate(costs):
             if i < n:
-                cost += ele[1]
+                cost += ele[0]
             else:
-                cost += ele[2]
+                cost += ele[1]
         
         return cost

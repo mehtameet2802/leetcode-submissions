c9fb8228f1ad47af28c1bdfac9e034b693e4f47c
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-02 19:55:29 +0530

    Time: 3 ms (59.36%), Space: 20.2 MB (99.33%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,21 +1,27 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
+        # '''
+        # Pattern - SUM Property
+
+        # TC - O(N)
+        # SC - O(1)
+        # '''
+
+        # ans = sum(nums)
+        # n = len(nums)
+
+        # total = n*(n+1)//2
+        # ans = total-ans
+        
+        # return ans
+
         '''
         Pattern - XOR
 
         TC - O(N)
         SC - O(1)
         '''
-
-        ans = sum(nums)
-        n = len(nums)
-
-        total = n*(n+1)//2
-        ans = total-ans
-        
-        return ans
-
         
         ans = len(nums)
 

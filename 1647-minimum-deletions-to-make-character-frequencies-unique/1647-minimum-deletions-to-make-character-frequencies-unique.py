bd56228807667db9798f35db5d586d0b68acfbfe
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-02 19:13:18 +0530

    Time: 66 ms (80.3%), Space: 20.4 MB (28.54%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        f_map = Counter(s)
+
+        # freq = sorted(f_map.values())
+
+        seen = set()
+        ans = 0
+
+        for i in f_map.values():
+            if i in seen:
+                while i>0 and i in seen:
+                    i -= 1
+                    ans += 1
+            seen.add(i)
+        
+        return ans

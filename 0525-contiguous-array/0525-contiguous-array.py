9ea9def16fc398b635eaff571be653b95de830ec
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-02 20:53:29 +0530

    Time: 79 ms (53.56%), Space: 25.3 MB (31.03%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.py
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -1,24 +1,26 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        '''
+        Pattern - Prefix Sum
+
+        TC - O(N)
+        SC - O(N)
+        '''
+
         idx_map = {}
-
-        pre_sum = [0]*len(nums)
-
-        for i in range(len(nums)):
-            if nums[i] == 0:
-                nums[i] = -1
         
-        sum = 0
-        for i in range(len(nums)):
-            pre_sum[i] = sum+nums[i]
-            sum = pre_sum[i]
-        
+        prefix = 0
         ans = 0
         idx_map[0] = -1
-        for i,num in enumerate(pre_sum):
-            if num in idx_map:
-                ans = max(ans,i-idx_map[num])
+        for i,num in enumerate(nums):
+            if num == 0:
+                prefix -= 1
             else:
-                idx_map[num] = i  
+                prefix += 1
+
+            if prefix in idx_map:
+                ans = max(ans,i-idx_map[prefix])
+            else:
+                idx_map[prefix] = i  
         
         return ans

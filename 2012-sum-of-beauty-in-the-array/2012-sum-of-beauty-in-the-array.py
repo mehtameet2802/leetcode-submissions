52c4a64e45b6ee103af00ec3d0c76527cd2c3373
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-03 18:57:01 +0530

    Time: 107 ms (95.29%), Space: 33.3 MB (22.94%) - LeetHub

---
+++ 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sumOfBeauties(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+        pre = [0]*n
+        pre_max = -float('inf')
+        suf_min = float('inf')
+        ans = 0
+
+        for i in range(n-1):
+            pre[i] = pre_max
+            pre_max = max(pre_max,nums[i])
+        pre[n-1] = pre_max
+        
+        for i in range(n-2,0,-1):
+            suf_min = min(nums[i+1], suf_min)
+            if nums[i] > pre[i] and nums[i] < suf_min:
+                ans += 2
+            elif nums[i] > nums[i-1] and nums[i] < nums[i+1]:
+                ans += 1
+
+        return ans
+

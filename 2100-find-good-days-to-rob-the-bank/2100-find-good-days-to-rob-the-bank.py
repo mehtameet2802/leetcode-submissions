84c168f2295583438eb5798c8f202a444462fa38
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-03 18:35:56 +0530

    Time: 95 ms (78.21%), Space: 35.2 MB (97.76%) - LeetHub

--- 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
@@ -1,11 +1,59 @@
 class Solution:
     def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
+
+        # '''
+        # Pattern - Prefix + Suffix Count
+        
+        # TC - O(N)
+        # SC - O(N)
+        # '''
+
+        # pre = 0
+        # suf = 0
+        # n = len(security)
+
+        # pre_days = [0]*n
+        # suf_days = [0]*n
+
+        # for i in range(n-1):
+        #     pre_days[i] = pre
+        #     if security[i] >= security[i+1]:
+        #         pre += 1
+        #     else:
+        #         pre = 0
+        
+        # pre_days[n-1] = pre
+    
+
+        # for i in range(n-1,0,-1):
+        #     suf_days[i] = suf
+        #     if security[i] >= security[i-1]:
+        #         suf += 1
+        #     else:
+        #         suf = 0
+        
+        # suf_days[0] = suf
+
+        # ans = []
+        # for i in range(n):
+        #     if pre_days[i] - time >= 0 and suf_days[i] - time >= 0:
+        #         ans.append(i)
+        
+        # return ans
+
+
+        '''
+        Pattern - Prefix + Suffix Count
+        
+        TC - O(N)
+        SC - O(N)
+        '''
+
         pre = 0
         suf = 0
         n = len(security)
 
         pre_days = [0]*n
-        suf_days = [0]*n
 
         for i in range(n-1):
             pre_days[i] = pre
@@ -15,20 +63,20 @@ class Solution:
                 pre = 0
         
         pre_days[n-1] = pre
-    
-
+            
+        
+        ans = []
         for i in range(n-1,0,-1):
-            suf_days[i] = suf
+            if pre_days[i] >= time and suf >= time:
+                ans.append(i)
+
             if security[i] >= security[i-1]:
                 suf += 1
             else:
                 suf = 0
         
-        suf_days[0] = suf
+        if pre_days[0] >= time and suf >= time:
+            ans.append(0)
 
-        ans = []
-        for i in range(n):
-            if pre_days[i] - time >= 0 and suf_days[i] - time >= 0:
-                ans.append(i)
         
         return ans

9910b38db51cb09b044117bd44866b8370a9c104
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-03 14:44:37 +0530

    Time: 45 ms (45.96%), Space: 31.6 MB (22.49%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
@@ -1,12 +1,18 @@
 class Solution:
     def maxAbsoluteSum(self, nums: List[int]) -> int:
         
+        '''
+        Pattern - Kadane Algorithm
+
+        TC - O(N)
+        SC - O(1)
+        '''
         
         cur_max = cur_min = nums[0]
-        ans = nums[0]
+        ans = abs(nums[0])
 
         for num in nums[1:]:
-            ans = max(ans, abs(num), abs(cur_max), abs(cur_min))
             cur_max, cur_min = max(num, num+cur_max), min(num, num+cur_min)
+            ans = max(ans, abs(num), abs(cur_max), abs(cur_min))
         
-        return max(ans,abs(cur_max),abs(cur_min))
+        return ans

d57c8710f8af9b31c101e6691b20045da7e41eb9
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-03 14:39:54 +0530

    Time: 49 ms (29.51%), Space: 29.8 MB (99.86%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.py
@@ -6,8 +6,7 @@ class Solution:
         ans = nums[0]
 
         for num in nums[1:]:
-            ans = max(ans, abs(num), abs(num+cur_max), abs(num+cur_min))
+            ans = max(ans, abs(num), abs(cur_max), abs(cur_min))
             cur_max, cur_min = max(num, num+cur_max), min(num, num+cur_min)
-            
         
         return max(ans,abs(cur_max),abs(cur_min))

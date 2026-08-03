b456667f32d0f1b4e71c7e85488541d0db58d214
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-03 15:50:34 +0530

    Time: 56 ms (52.69%), Space: 36.2 MB (62.37%) - LeetHub

--- 1734-decode-xored-permutation/1734-decode-xored-permutation.py
+++ 1734-decode-xored-permutation/1734-decode-xored-permutation.py
@@ -1,5 +1,13 @@
 class Solution:
     def decode(self, encoded: List[int]) -> List[int]:
+
+        '''
+        Pattern - Prefix XOR
+
+        TC - O(N+1)
+        SC - O(1)
+        '''
+
         n = len(encoded) + 1
 
         total = 0
@@ -7,7 +15,7 @@ class Solution:
             total = total^i
 
         odd = 0
-        for i in range(1,n,2):
+        for i in range(1,n-1,2):
             odd = odd ^ encoded[i]
         
         first = total ^ odd

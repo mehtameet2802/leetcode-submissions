aab01462a0094597e4573444d2c07d681bf6a643
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-03 16:11:09 +0530

    Time: 0 ms (100%), Space: 19.2 MB (89.1%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.py
@@ -1,19 +1,47 @@
 class Solution:
     def findMiddleIndex(self, nums: List[int]) -> int:
-        pre_sum = [0]*len(nums)
-        suf_sum = [0]*len(nums)
+
+        # '''
+        # Pattern - Pre + Suf Sum
+
+        # TC - O(N)
+        # SC - O(N)
+        # '''
+
+        # pre_sum = [0]*len(nums)
+        # suf_sum = [0]*len(nums)
+
+        # n = len(nums)
+        # for i in range(1,n):
+        #     pre_sum[i] = nums[i-1] + pre_sum[i-1]
+        
+        # for i in range(n-2,-1,-1):
+        #     suf_sum[i] = suf_sum[i+1] + nums[i+1]
+        
+        # for i in range(n):
+        #     if pre_sum[i] == suf_sum[i]:
+        #         return i
+        
+        # return -1
+
+
+        '''
+        Pattern - Pre + Suf Sum
+
+        TC - O(N)
+        SC - O(1)
+        '''
 
         n = len(nums)
-        for i in range(1,n):
-            pre_sum[i] = nums[i-1] + pre_sum[i-1]
+        pre_sum = 0
+        total = sum(nums)
         
-        for i in range(n-2,-1,-1):
-            suf_sum[i] = suf_sum[i+1] + nums[i+1]
-        
-        print(pre_sum)
-        print(suf_sum)
         for i in range(n):
-            if pre_sum[i] == suf_sum[i]:
+            suf_sum = total - pre_sum - nums[i]
+            if pre_sum == suf_sum:
                 return i
+            
+            pre_sum = pre_sum + nums[i]
         
         return -1
+

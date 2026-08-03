050cfc31c17881257411d35f347f04d696f432e5
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-03 14:59:09 +0530

    Time: 101 ms (67.55%), Space: 27.5 MB (5.98%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,17 +1,25 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        
+        '''
+        Pattern - Prefix Sum
+
+        TC - O(N)
+        SC - O(k) - number of frequent odd numbers
+        '''
+        
         seen = defaultdict(int)
 
         seen[0] = 1
 
-        cnt = 0
+        prefix = 0
         ans = 0
         for num in nums:
             
             if num % 2 != 0:
-                cnt += 1
-            seen[cnt] += 1
-            ans += seen[cnt-k]
-
+                prefix += 1
+            ans += seen[prefix-k]
+            seen[prefix] += 1
+            
         return ans
 

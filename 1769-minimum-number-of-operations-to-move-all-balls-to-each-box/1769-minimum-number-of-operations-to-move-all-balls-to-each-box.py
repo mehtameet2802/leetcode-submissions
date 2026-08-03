6ea0417a106d36f51603d205a2daceda7ce674bd
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-03 19:20:01 +0530

    Time: 19 ms (56.72%), Space: 19.8 MB (8.2%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,8 +1,16 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
+
+        '''
+        Pattern - Prefix and Suffix Count and Sum
+
+        TC - O(N)
+        SC - O(N)
+        '''
+
         n = len(boxes)
         pre = [[0,0] for _ in range(n)]
-        suf = [[0,0] for _ in range(n)]
+        suf = [0,0]
 
         for i in range(1,n):
             prev = pre[i-1]
@@ -10,18 +18,15 @@ class Solution:
             if boxes[i-1] == '1':
                 new = [new[0]+1, new[1]+1]
             pre[i] = new
-        
-        for i in range(n-2,-1,-1):
-            prev = suf[i+1]
-            new = [prev[0]+prev[1], prev[1]]
-
-            if boxes[i+1] == '1':
-                new = [new[0]+1, new[1]+1]
-            
-            suf[i] = new
-        
+                
         ans = []
-        for i in range(n):
-            ans.append(pre[i][0] + suf[i][0])
+        for i in range(n-1,-1,-1):
+            ans.append(pre[i][0] + suf[0])
+            new = [suf[0]+suf[1], suf[1]]
+
+            if boxes[i] == '1':
+                suf = [new[0]+1,new[1]+1]
+            else:
+                suf = new
         
-        return ans
+        return ans[::-1]

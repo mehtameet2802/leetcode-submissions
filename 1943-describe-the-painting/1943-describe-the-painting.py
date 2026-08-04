df09954c39f1a481d280582e22ed867776618aba
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-04 12:03:13 +0530

    Time: 98 ms (63.59%), Space: 37.4 MB (65.05%) - LeetHub

---
+++ 1943-describe-the-painting/1943-describe-the-painting.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+
+class Solution:
+    def splitPainting(self, segments: List[List[int]]) -> List[List[int]]:
+        events = defaultdict(int)
+
+        for s,e,color in segments:
+            events[s] += color
+            events[e] -= color 
+        
+        points = sorted(events.keys())
+
+        ans = []
+        cur = 0
+        for i in range(len(points)-1):
+            cur += events[points[i]]
+
+            if cur > 0:
+                ans.append([points[i], points[i+1], cur])
+
+        return ans

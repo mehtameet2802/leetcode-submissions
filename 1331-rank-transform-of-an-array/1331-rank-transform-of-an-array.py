89fbf67421a7c89d6376a6f69fba3076a930d413
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-04 12:42:37 +0530

    Time: 43 ms (51.67%), Space: 37.5 MB (65.21%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,11 +1,16 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        '''
+        TC - O(U log U)
+        SC - O(U) - Unique elements
+        '''
+
         rank = {}
 
         for i, num in enumerate(sorted(set(arr)),1):
             rank[num] = i
 
         for i, num in enumerate(arr):
-            arr[i] = rank[arr[i]]
+            arr[i] = rank[num]
         
         return arr

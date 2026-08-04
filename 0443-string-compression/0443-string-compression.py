2e88e8da074e5a532e6673b8de8dab19ff12a516
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-04 18:37:20 +0530

    Time: 4 ms (14.79%), Space: 19.4 MB (55.27%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,26 +1,30 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
+        '''
+        Pattern - 2 pointer
+        TC - O(N)
+        SC - O(1)
+        '''
+
         l = 0
         r = 0
         w = 0
         n = len(chars)
 
-
-        cnt = 0
-        ans = []
-
         while r<n:
             while r<n and chars[r] == chars[l]:
                 r += 1
-                cnt += 1
             
-            count = str(cnt)
+            
             chars[w] = chars[l]
             w+=1
-            if cnt > 1:
-                for ch in count:
+
+            count = r - l
+            if count > 1:
+                for ch in str(count):
                     chars[w] = ch
                     w += 1
+                    
             cnt = 0
             l = r
         

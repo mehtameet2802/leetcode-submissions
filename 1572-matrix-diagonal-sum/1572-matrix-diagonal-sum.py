09cef5616be0201ae41668d6c47b2f3718b4b2aa
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-04 14:59:10 +0530

    Time: 0 ms (100%), Space: 19.6 MB (53.26%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -1,24 +1,54 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
+
+        # '''
+        # Pattern - Matrix Traversal
+        
+        # TC - O(N)
+        # SC - O(1)
+        # '''
+
+        # ans = 0
+        # r = len(mat)
+        # c = len(mat[0])
+
+        # i = j = 0
+        # while i < r:
+        #     ans += mat[i][j]
+        #     i += 1
+        #     j += 1
+        
+        # i = 0
+        # j = c-1
+        # while i < r:
+        #     if i == j:
+        #         i += 1
+        #         j -= 1
+        #         continue
+        #     ans += mat[i][j]
+        #     i += 1
+        #     j -= 1
+        
+        # return ans
+
+
+
+        '''
+        Pattern - Matrix Traversal
+        
+        TC - O(N)
+        SC - O(1)
+        '''
+
         ans = 0
         r = len(mat)
-        c = len(mat[0])
 
-        i = j = 0
-        while i < r:
-            ans += mat[i][j]
+        for i in range(r):
+            ans += mat[i][i]
+            ans += mat[i][r-i-1]
             i += 1
-            j += 1
-        
-        i = 0
-        j = c-1
-        while i < r:
-            if i == j:
-                i += 1
-                j -= 1
-                continue
-            ans += mat[i][j]
-            i += 1
-            j -= 1
-        
+
+        if i%2 != 0:
+            ans -= mat[r//2][r//2]
+     
         return ans

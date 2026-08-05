efa02636d5799a1e2e8a67904b8e750415f82fc2
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-05 21:45:01 +0530

    Time: 61 ms (45.19%), Space: 22.1 MB (85.25%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -1,5 +1,12 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
+
+        '''
+        Pattern - Sliding Window
+
+        TC - O(N)
+        SC - 0(1)
+        '''
         
         left = 0
         ans = 0

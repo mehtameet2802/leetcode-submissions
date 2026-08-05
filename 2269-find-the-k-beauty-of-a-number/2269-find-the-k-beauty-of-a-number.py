efb1dfcf1fa7286c37b4826561bf24e406d84329
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-05 17:03:26 +0530

    Time: 0 ms (100%), Space: 19.2 MB (88.72%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.py
@@ -1,10 +1,19 @@
 class Solution:
     def divisorSubstrings(self, num: int, k: int) -> int:
+        
+        '''
+        Pattern - FIxed Window
+
+        TC - O(N)
+        SC - O(1)
+        '''
+
         number = str(num)
 
         left = 0
         ans = 0
         cur = 0
+        power = pow(10,k-1)
 
         for right in range(len(number)):
             cur = cur*10 + int(number[right])
@@ -14,6 +23,6 @@ class Solution:
                 if cur > 0 and num % cur == 0:
                     ans += 1
                 
-                cur -= (int(number[left]) * pow(10,k-1))
+                cur -= (int(number[left]) * power)
                 left += 1
         return ans

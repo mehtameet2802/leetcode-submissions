b19c078fba81b8191e7c6a26535cae2c7edf44e1
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-05 23:27:38 +0530

    Time: 25 ms (34.16%), Space: 19.8 MB (43.03%) - LeetHub

--- 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
+++ 1208-get-equal-substrings-within-budget/1208-get-equal-substrings-within-budget.py
@@ -1,6 +1,13 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+
+        '''
+        Pattern - Sliding Window
         
+        TC - O(N)
+        SC - O(1)
+        '''
+
         left = 0
         ans = 0
         cost = 0

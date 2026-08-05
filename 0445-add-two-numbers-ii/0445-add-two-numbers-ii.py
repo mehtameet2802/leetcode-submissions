91050b9e6b5e7fb8bb15113a8ca85286b147d601
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-06 00:55:14 +0530

    Time: 3 ms (77.35%), Space: 19.2 MB (96.34%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -16,33 +16,35 @@ class Solution:
             stack2.append(l2.val)
             l2 = l2.next
         
-        stack3 = []
+
+        temp = None
+
         carry = 0
         while stack1 and stack2:
             val = stack1.pop() + stack2.pop() + carry
-            stack3.append(val%10)
+            new_node = ListNode(val%10)
+            new_node.next = temp
+            temp = new_node 
             carry = val // 10
         
         while stack1:
             val = stack1.pop() + carry
-            stack3.append(val%10)
+            new_node = ListNode(val%10)
+            new_node.next = temp
+            temp = new_node 
             carry = val // 10
         
         while stack2:
             val = stack2.pop() + carry
-            stack3.append(val%10)
+            new_node = ListNode(val%10)
+            new_node.next = temp
+            temp = new_node
             carry = val // 10
         
         if carry:
-            stack3.append(carry)
+            new_node = ListNode(carry)
+            new_node.next = temp
+            temp = new_node
         
-        dummy = ListNode()
-        temp = dummy
-
-        while stack3:
-            temp.next = ListNode(stack3.pop())
-            temp = temp.next
-        
-        return dummy.next
-
+        return temp
         

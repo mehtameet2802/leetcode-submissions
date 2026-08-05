364cc18091c355e06cf686cfad34e1615ef91e46
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-05 16:48:23 +0530

    Time: 3 ms (36.38%), Space: 18.6 MB (100%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.py
@@ -1,6 +1,39 @@
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
 
+        # '''
+        # Pattern - Fixed Window
+
+        # TC - O(N)
+        # SC - O(1)
+        # '''
+        
+        # left = 0
+        # window = 3
+        # ans = 0
+        # seen = [0]*26
+
+        # for right in range(len(s)):
+
+        #     if seen[ord(s[right])-ord('a')] > 0:
+        #         while s[left] != s[right]:
+        #             seen[ord(s[left])-ord('a')] -= 1
+        #             left += 1
+        #         seen[ord(s[left])-ord('a')] -= 1
+        #         left += 1
+                
+            
+        #     seen[ord(s[right])-ord('a')] += 1
+
+        #     if right - left + 1 == window:
+        #         ans += 1
+                
+        #         seen[ord(s[left])-ord('a')] -= 1
+        #         left += 1
+
+        # return ans
+
+
         '''
         Pattern - Fixed Window
 
@@ -15,18 +48,12 @@ class Solution:
 
         for right in range(len(s)):
 
-            if seen[ord(s[right])-ord('a')] > 0:
-                while s[left] != s[right]:
-                    seen[ord(s[left])-ord('a')] -= 1
-                    left += 1
-                seen[ord(s[left])-ord('a')] -= 1
-                left += 1
-                
-            
             seen[ord(s[right])-ord('a')] += 1
 
             if right - left + 1 == window:
-                ans += 1
+                
+                if max(seen) == 1:
+                    ans += 1
                 
                 seen[ord(s[left])-ord('a')] -= 1
                 left += 1

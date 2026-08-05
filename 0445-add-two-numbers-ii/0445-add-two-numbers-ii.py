ca8ef85615b1cccb80d9a14b351313af61f4bbd8
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-06 00:57:08 +0530

    Time: 2 ms (82.11%), Space: 19.4 MB (13.16%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -5,6 +5,50 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # stack1 = []
+        # stack2 = []
+
+        # while l1:
+        #     stack1.append(l1.val)
+        #     l1 = l1.next
+        
+        # while l2:
+        #     stack2.append(l2.val)
+        #     l2 = l2.next
+        
+
+        # temp = None
+
+        # carry = 0
+        # while stack1 and stack2:
+        #     val = stack1.pop() + stack2.pop() + carry
+        #     new_node = ListNode(val%10)
+        #     new_node.next = temp
+        #     temp = new_node 
+        #     carry = val // 10
+        
+        # while stack1:
+        #     val = stack1.pop() + carry
+        #     new_node = ListNode(val%10)
+        #     new_node.next = temp
+        #     temp = new_node 
+        #     carry = val // 10
+        
+        # while stack2:
+        #     val = stack2.pop() + carry
+        #     new_node = ListNode(val%10)
+        #     new_node.next = temp
+        #     temp = new_node
+        #     carry = val // 10
+        
+        # if carry:
+        #     new_node = ListNode(carry)
+        #     new_node.next = temp
+        #     temp = new_node
+        
+        # return temp
+        
+
         stack1 = []
         stack2 = []
 
@@ -20,31 +64,16 @@ class Solution:
         temp = None
 
         carry = 0
-        while stack1 and stack2:
-            val = stack1.pop() + stack2.pop() + carry
-            new_node = ListNode(val%10)
-            new_node.next = temp
-            temp = new_node 
-            carry = val // 10
-        
-        while stack1:
-            val = stack1.pop() + carry
-            new_node = ListNode(val%10)
-            new_node.next = temp
-            temp = new_node 
-            carry = val // 10
-        
-        while stack2:
-            val = stack2.pop() + carry
-            new_node = ListNode(val%10)
+        while stack1 or stack2 or carry:
+            val1 = stack1.pop() if stack1 else 0
+            val2 = stack2.pop() if stack2  else 0
+
+            total = val1 + val2 + carry
+
+            new_node = ListNode(total % 10)
             new_node.next = temp
             temp = new_node
-            carry = val // 10
-        
-        if carry:
-            new_node = ListNode(carry)
-            new_node.next = temp
-            temp = new_node
+
+            carry = total // 10
         
         return temp
-        

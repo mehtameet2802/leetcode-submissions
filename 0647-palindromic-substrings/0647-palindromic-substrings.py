b94ea5b9277f8248464a193bf0d4a48a318d8644
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-05 14:45:20 +0530

    Time: 121 ms (66.2%), Space: 19.3 MB (37.93%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,29 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        '''
+        Pattern - Exoand Around the Center
+
+        TC - 
+        SC - O(1)
+        '''
+
+        def expand(i,j):
+            
+            cnt = 0
+
+            while i>=0 and j<len(s) and s[i] == s[j]:
+                i -= 1
+                j += 1
+
+                cnt += 1
+            
+            return cnt
+
+
+
+        ans = 0
+        for i in range(len(s)):
+            ans += expand(i,i)
+            ans += expand(i,i+1)
+        
+        return ans

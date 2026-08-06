eea9f224a3c3407b3e5fdd788d9a39bde57f4e93
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-06 15:45:53 +0530

    Time: 0 ms (100%), Space: 19.4 MB (16.81%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         '''
-        Pattern - Custom Sort
+        Pattern - HashSet + Custom Sort
 
         U - number of unique N
 

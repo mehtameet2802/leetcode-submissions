d931a5fcb5bfe6b0c5d56d4fe1df48862f6cd47c
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-06 17:23:49 +0530

    Time: 34 ms (74.29%), Space: 19.4 MB (24.44%) - LeetHub

---
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkArithmeticSubarrays(self, nums: List[int], l: List[int], r: List[int]) -> List[bool]:
+        '''
+        Pattern - Sort + Scan
+
+        TC - O(M log M)
+        SC - O(1)
+        '''
+
+        ans = []
+
+        for s, e in zip(l,r):
+        
+            arr = nums[s:e+1]
+            arr.sort()
+            diff = arr[1] - arr[0]
+            res = True
+
+            for i in range(2,len(arr)):
+                if arr[i] - arr[i-1] != diff:
+                    res = False
+                    break
+            
+            ans.append(res)
+            
+        return ans

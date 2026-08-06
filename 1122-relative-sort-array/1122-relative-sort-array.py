7d1b8d63931b5f77c194297ff698d8f3b0046e92
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-06 15:51:31 +0530

    Time: 1 ms (50.6%), Space: 19.3 MB (84.83%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,31 +1,55 @@
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        # '''
+        # Pattern - HashSet + Custom Sort
+
+        # U - number of unique N
+
+        # TC - O(N + U log U)
+        # SC - O(U)
+        # '''
+
+        # ans = []
+
+        # f_map = Counter(arr1)
+
+        # for num in arr2:
+        #     while f_map[num] > 0:
+        #         ans.append(num)
+        #         f_map[num] -= 1
+            
+        #     del f_map[num]
+        
+        # for num in sorted(f_map.keys()):
+        #     while f_map[num] > 0:
+        #         ans.append(num)
+        #         f_map[num] -= 1
+            
+        #     del f_map[num]
+        
+        # return ans
+        
         '''
         Pattern - HashSet + Custom Sort
 
         U - number of unique N
 
-        TC - O(N + U log U)
-        SC - O(U)
+        TC - O(N)
+        SC - O(1)
         '''
 
         ans = []
+        f_arr = [0]*1001
 
-        f_map = Counter(arr1)
+        for num in arr1:
+            f_arr[num] += 1
 
         for num in arr2:
-            while f_map[num] > 0:
-                ans.append(num)
-                f_map[num] -= 1
-            
-            del f_map[num]
+            ans.extend([num] * f_arr[num])
+            f_arr[num] = 0
         
-        for num in sorted(f_map.keys()):
-            while f_map[num] > 0:
-                ans.append(num)
-                f_map[num] -= 1
-            
-            del f_map[num]
-        
+        for i, num in enumerate(f_arr):
+            ans.extend([i]*num)
+
         return ans
         

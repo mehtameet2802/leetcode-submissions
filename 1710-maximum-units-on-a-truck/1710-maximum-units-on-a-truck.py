25984ae5621e288b705cb0d955a3ea974be9bd31
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-06 19:02:47 +0530

    Time: 0 ms (100%), Space: 19.6 MB (47.1%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,20 +1,46 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+
+        # '''
+        # Pattern - Sort + Greedy
+
+        # TC - O(N log N)
+        # SC - O(1)
+        # '''
         
-        boxTypes.sort(key = lambda x : x[1])
-        r = len(boxTypes) - 1
+        # boxTypes.sort(key = lambda x : x[1])
+        # r = len(boxTypes) - 1
 
+        # ans = 0
+        # while r>=0 and truckSize>0:
+
+        #     if truckSize >= boxTypes[r][0]: 
+        #         ans += boxTypes[r][0] * boxTypes[r][1]
+        #         truckSize -= boxTypes[r][0]
+        #     else:
+        #         ans += boxTypes[r][1] * truckSize
+        #         truckSize = 0
+        #     r -= 1
+        
+        # return ans
+
+
+        '''
+        Pattern - Sort + Greedy
+
+        TC - O(N log N)
+        SC - O(1)
+        '''
+
+        boxTypes.sort(key=lambda x: x[1], reverse=True)
         ans = 0
-        while r>=0 and truckSize>0:
 
-            if truckSize >= boxTypes[r][0]: 
-                ans += boxTypes[r][0] * boxTypes[r][1]
-                truckSize -= boxTypes[r][0]
-            else:
-                ans += boxTypes[r][1] * truckSize
-                truckSize = 0
-            r -= 1
+        for boxes, units in boxTypes:
+            take = min(boxes, truckSize)
+            ans += take * units
+            truckSize -= take
+
+            if truckSize == 0:
+                break
         
         return ans
-
-51

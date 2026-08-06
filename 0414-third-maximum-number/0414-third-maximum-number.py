7bd479d0a2eccd621400c2bf58f72427443492d9
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-06 15:00:31 +0530

    Time: 0 ms (100%), Space: 19.7 MB (73.24%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -1,26 +1,54 @@
 class Solution:
     def thirdMax(self, nums: List[int]) -> int:
+        # '''
+        # Pattern - Sort + Scan
+
+        # TC - O(N log N)
+        # SC - O(1)
+        # '''
+
+        # n = len(nums)
+        # if n<3:
+        #     return max(nums)
+        
+        # nums.sort()
+
+        # cnt = 0
+        # for i in range(n-1,-1,-1):
+        #     if i<n-1 and nums[i] == nums[i+1]:
+        #         continue
+        #     cnt += 1
+        #     if cnt == 3:
+        #         return nums[i]
+            
+        # if cnt < 3:
+        #     return nums[-1]
+
         '''
         Pattern - Sort + Scan
 
-        TC - O(N log N)
+        TC - O(N)
         SC - O(1)
         '''
 
-        n = len(nums)
-        if n<3:
-            return max(nums)
-        
-        nums.sort()
+        first = second = third = None
 
-        cnt = 0
-        for i in range(n-1,-1,-1):
-            if i<n-1 and nums[i] == nums[i+1]:
+        for num in nums:
+
+            if num == first or num == second or num == third:
                 continue
-            cnt += 1
-            if cnt == 3:
-                return nums[i]
             
-        if cnt < 3:
-            return nums[-1]
-
+            if first is None or num > first:
+                third = second
+                second = first
+                first = num
+            
+            elif second is None or num > second:
+                third = second
+                second = num
+            
+            elif third is None or num > third:
+                third = num
+            
+        
+        return first if third is None else third

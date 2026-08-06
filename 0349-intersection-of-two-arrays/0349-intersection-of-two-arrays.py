bccbbbb4641ac76fa45e67320775968b6367d88c
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-06 14:32:31 +0530

    Time: 1 ms (40.52%), Space: 19.4 MB (43.98%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -1,31 +1,52 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # '''
+        # Pattern - Sort + Scan
+
+        # TC - O(N log N)
+        # SC - O(K)
+        # '''
+
+        # nums1.sort()
+        # nums2.sort()
+
+        # ans = []
+        # p1 = p2 = 0
+
+        # while p1<len(nums1) and p2<len(nums2):
+        #     if nums1[p1] == nums2[p2]:
+        #         if p1 > 0 and nums1[p1] == nums1[p1-1]:
+        #             p1 += 1
+        #             p2 += 1
+        #             continue
+                    
+        #         ans.append(nums1[p1])
+        #         p1 += 1
+        #         p2 += 1
+        #     elif nums1[p1] < nums2[p2]:
+        #         p1 += 1
+        #     else:
+        #         p2 += 1
+        
+        # return ans
+
         '''
         Pattern - Sort + Scan
 
-        TC - O(N log N)
-        SC - O(K)
+        TC - O(N)
+        SC - O(N)
         '''
 
-        nums1.sort()
-        nums2.sort()
+        seen = set()
 
-        ans = []
-        p1 = p2 = 0
+        ans = set()
 
-        while p1<len(nums1) and p2<len(nums2):
-            if nums1[p1] == nums2[p2]:
-                if p1 > 0 and nums1[p1] == nums1[p1-1]:
-                    p1 += 1
-                    p2 += 1
-                    continue
-                    
-                ans.append(nums1[p1])
-                p1 += 1
-                p2 += 1
-            elif nums1[p1] < nums2[p2]:
-                p1 += 1
-            else:
-                p2 += 1
+        for num in nums1:
+            seen.add(num)
         
-        return ans
+        for num in nums2:
+            if num in seen:
+                ans.add(num)
+        
+        return list(ans)
+

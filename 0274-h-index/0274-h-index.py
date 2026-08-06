8842ec3db0dc1d51e85062694787c61e12f3c731
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-06 16:06:43 +0530

    Time: 0 ms (100%), Space: 19.5 MB (25.37%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,19 +1,52 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        citations.sort()
+
+        # '''
+        # Pattern - Sort + Scan
+
+        # TC - O(N log N)
+        # SC - O(1)
+        # '''
+
+        # citations.sort()
+
+        # n = len(citations)
+        # l = 0
+        # r = n - 1
+
+        # while l<=r:
+        #     papers = r - l + 1
+
+        #     if citations[l] >= papers:
+        #         return r - l +1
+        #     else:
+        #         l += 1
+        
+        # return 0
+
+
+        '''
+        Pattern - Bucket Sort
+
+        TC - O(N)
+        SC - O(N)
+        '''
 
         n = len(citations)
-        l = 0
-        r = n - 1
+        buckets = n+1
+        count_arr = [0]*buckets
 
-        ans = 0 
-
-        while l<=r:
-            papers = r - l + 1
-
-            if citations[l] >= papers:
-                return r - l +1
+        for c in citations:
+            if c >= n:
+                count_arr[n] += 1
             else:
-                l += 1
+                count_arr[c] += 1
+
+        papers = 0
+        for h in range(n,-1,-1):
+            papers += count_arr[h] 
+
+            if papers >= h:
+                return h
         
         return 0

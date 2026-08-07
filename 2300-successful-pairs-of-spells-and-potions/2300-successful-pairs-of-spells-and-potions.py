c59d16ad3775c1ccd425d576fcb1434f5d041473
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-07 23:11:25 +0530

    Time: 262 ms (76.57%), Space: 46.1 MB (9.74%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -2,6 +2,12 @@ import math
 from bisect import bisect_left
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        '''
+        Pattern - lower bound
+        TC - O(M log M + N log M)
+        SC - O(1)
+        '''
+
         potions.sort()
         ans = []
         n = len(potions)

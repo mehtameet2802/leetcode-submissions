573696a101b09d78455340795536f6b1957cb450
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-07 19:04:50 +0530

    Time: 0 ms (100%), Space: 19.8 MB (9.89%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        '''
+        Pattern - Binary Search
+        TC - O(log n)
+        SC - O(1)
+        '''
+        l = 0
+        r = len(nums) - 1
+
+        while l<=r:
+            mid = l + (r-l)//2
+
+            if nums[mid] == target:
+                return True
+            elif nums[l] == nums[mid] == nums[r]:
+                l += 1
+                r -= 1
+            elif nums[l]<=nums[mid] and nums[r] < nums[mid]:
+                if nums[l] <= target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[r]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            
+        return False

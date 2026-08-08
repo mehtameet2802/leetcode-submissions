847c0b728ea79a1de5ecbe7c1c27fcc5b673a370
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-08 18:32:33 +0530

    Time: 1 ms (79.05%), Space: 19.3 MB (29.38%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,24 +1,60 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        open = 0
-        skipped = 0
-        stack = []
+        # '''
+        # Pattern - Stack / Parentheses Depth
+
+        # TC - O(N)
+        # SC - O(N)
+        # '''
+
+        # open = 0
+        # skipped = 0
+        # stack = []
+
+        # for ch in s:
+        #     if open == 0 and skipped == 0 and ch == '(':
+        #         skipped += 1
+        #         continue
+            
+        #     if open == 0 and skipped == 1 and ch == ')':
+        #         skipped -= 1
+        #         continue
+            
+        #     if open>0 and ch == ')':
+        #         open -= 1
+        #     else:
+        #         open += 1
+            
+        #     stack.append(ch)
+        
+        # return "".join(stack)
+
+
+        '''
+        Pattern - Stack / Parentheses Depth
+
+        TC - O(N)
+        SC - O(N)
+        '''
+
+        depth = 0
+        ans = []
 
         for ch in s:
-            if open == 0 and skipped == 0 and ch == '(':
-                skipped += 1
-                continue
-            
-            if open == 0 and skipped == 1 and ch == ')':
-                skipped -= 1
-                continue
-            
-            if open>0 and ch == ')':
-                open -= 1
+            if ch == '(':
+                if depth > 0:
+                    ans.append(ch)
+                
+                depth += 1
             else:
-                open += 1
-            
-            stack.append(ch)
+                depth -= 1
+                
+                if depth > 0:
+                    ans.append(ch)
+                
+                
         
-        return "".join(stack)
+        return "".join(ans)
+
+
 

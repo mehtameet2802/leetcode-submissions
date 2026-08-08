60cc7bcf27b775008984745f73a6473ccbde8817
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-09 02:24:52 +0530

    Time: 0 ms (100%), Space: 19.3 MB (92.63%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,23 +1,41 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        stack = []
-        idx_map = {}
+        # stack = []
+        # idx_map = {}
 
-        for i,num in enumerate(nums2):
-            idx_map[num] = i
+        # for i,num in enumerate(nums2):
+        #     idx_map[num] = i
 
-            while stack and nums2[stack[-1]] < num:
-                j = stack.pop()
-                nums2[j] = num
+        #     while stack and nums2[stack[-1]] < num:
+        #         j = stack.pop()
+        #         nums2[j] = num
             
-            stack.append(i)
+        #     stack.append(i)
         
+
+        # for i, num in enumerate(nums1):
+        #     if nums2[idx_map[num]] == num:
+        #         nums1[i] = -1
+        #     else:
+        #         nums1[i] = nums2[idx_map[num]]
+        
+        # return nums1
+        
+
+        stack = []
+        greater = {}
+
+        for num in nums2:
+
+            while stack and stack[-1] < num:
+                greater[stack.pop()] = num
+            
+            stack.append(num)
+        
+        while stack:
+            greater[stack.pop()] = -1
 
         for i, num in enumerate(nums1):
-            if nums2[idx_map[num]] == num:
-                nums1[i] = -1
-            else:
-                nums1[i] = nums2[idx_map[num]]
+            nums1[i] = greater[num]
         
         return nums1
-        

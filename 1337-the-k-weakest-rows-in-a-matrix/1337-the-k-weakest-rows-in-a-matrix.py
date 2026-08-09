71b6745937cf08b3768fbaf3e06264d89d3d82e8
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-09 17:23:40 +0530

    Time: 0 ms (100%), Space: 19.7 MB (35.23%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -0,0 +1,17 @@
+import heapq
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        max_heap = []
+        
+        for i in range(len(mat)):
+            cnt_1 = sum(mat[i])
+            heapq.heappush(max_heap,(-cnt_1,-i))
+
+            if len(max_heap) > k:
+                heapq.heappop(max_heap)
+        
+        ans = [0]*k
+        for j in range(k-1,-1,-1):
+            ans[j] = (-heapq.heappop(max_heap)[1])
+        
+        return ans

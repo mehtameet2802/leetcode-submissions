1d49f096c2e7da50dfb467740b2bf56cd9746e9f
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-09 17:36:49 +0530

    Time: 0 ms (100%), Space: 19.8 MB (17.51%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -2,15 +2,54 @@ import heapq
 
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
+        # '''
+        # TC - O(N log N)
+        # SC - O(N)
+        # '''
+
+        # max_heap = []
+
+        # for gift in gifts:
+        #     heapq.heappush(max_heap,-gift)
+
+        # while k>0:
+        #     cur = -heapq.heappop(max_heap)
+        #     cur = int(pow(cur,0.5))
+        #     heapq.heappush(max_heap,-cur)
+        #     k -= 1
+        
+        # return -sum(max_heap)
+
+
+        '''
+        Pattern:
+        - Max Heap
+        - Repeatedly extract the maximum
+
+        TC - O(N + K log N)
+        SC - O(N)
+
+        heapify() → O(N)
+        Each operation:
+            pop maximum → O(log N)
+            push new value → O(log N)
+        K operations → O(K log N)
+        '''
         max_heap = []
 
         for gift in gifts:
-            heapq.heappush(max_heap,-gift)
+            max_heap.append(-gift)
 
-        while k>0:
+        heapq.heapify(max_heap)    
+
+        while k > 0:
             cur = -heapq.heappop(max_heap)
-            cur = int(pow(cur,0.5))
-            heapq.heappush(max_heap,-cur)
+
+            # Leave floor(sqrt(cur)) gifts
+            cur = isqrt(cur)
+
+            heapq.heappush(max_heap, -cur)
+
             k -= 1
-        
+
         return -sum(max_heap)

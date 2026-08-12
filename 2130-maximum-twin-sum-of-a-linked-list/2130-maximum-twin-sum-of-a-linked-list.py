852825031bbf656af87402234ab4857300d24679
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-12 20:15:05 +0530

    Time: 73 ms (29.67%), Space: 50.5 MB (63.35%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.py
@@ -5,28 +5,57 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        required = {}
-        i = 0
-        n = 0
+        # required = {}
+        # i = 0
+        # n = 0
 
-        cur = head
+        # cur = head
 
-        while cur:
-            n += 1
-            cur = cur.next
+        # while cur:
+        #     n += 1
+        #     cur = cur.next
             
-        ans = 0
-        limit = (n // 2) - 1
+        # ans = 0
+        # limit = (n // 2) - 1
 
-        while head:
-            if 0 <= i <= limit and 0 <= (n-i-1) < n :
-                required[n-i-1] = head.val
+        # while head:
+        #     if 0 <= i <= limit and 0 <= (n-i-1) < n :
+        #         required[n-i-1] = head.val
             
-            if i in required:
-                ans = max(ans,head.val+required[i])
+        #     if i in required:
+        #         ans = max(ans,head.val+required[i])
             
-            i += 1
-            head = head.next
+        #     i += 1
+        #     head = head.next
         
+        
+        # return ans
+
+        slow = head
+        fast = head.next
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        head2 = slow.next
+        slow.next = None
+        prev = None
+
+        while head2:
+            temp = head2.next
+            head2.next = prev
+            prev = head2
+            head2 = temp
+        
+        head2 = prev
+
+        ans = 0
+
+        while head and head2:
+            ans = max(ans, head.val+head2.val)
+            head = head.next
+            head2 = head2.next
         
         return ans
+

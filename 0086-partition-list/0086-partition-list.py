be7d2776fa14d777879fede6489cb1c40d94cad7
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-12 16:32:03 +0530

    Time: 0 ms (100%), Space: 19.3 MB (37.9%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -5,32 +5,65 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        dummy = ListNode()
-        dummy.next = head
+        # dummy = ListNode()
+        # dummy.next = head
+
+        # less = ListNode()
+        # temp = less
+        # prev = dummy
+
+        # while head:
+        #     if head.val < x:
+        #         new_node = head
+
+        #         prev.next = head.next
+        #         head = head.next
+
+        #         new_node.next = None
+        #         temp.next = new_node
+        #         temp = temp.next
+        #     else:
+        #         prev = head
+        #         head = head.next
+            
+        # if less.next:
+        #     temp.next = dummy.next
+        #     return less.next
+        
+        # return dummy.next
+
+        '''
+        Pattern:
+        - Linked List
+        - Two Chains
+
+        TC - O(N)
+        SC - O(1)
+        '''
 
         less = ListNode()
-        temp = less
-        prev = dummy
+        greater = ListNode()
+
+        less_ptr = less
+        greater_ptr = greater
 
         while head:
+
             if head.val < x:
-                new_node = head
+                less_ptr.next = head
+                less_ptr = less_ptr.next
 
-                prev.next = head.next
-                head = head.next
-
-                new_node.next = None
-                temp.next = new_node
-                temp = temp.next
             else:
-                prev = head
-                head = head.next
-            
-        if less.next:
-            temp.next = dummy.next
-            return less.next
-        
-        return dummy.next
+                greater_ptr.next = head
+                greater_ptr = greater_ptr.next
 
+            head = head.next
 
+        # End the greater/equal list
+        greater_ptr.next = None
+
+        # Connect the two lists
+        less_ptr.next = greater.next
+
+        return less.next
 

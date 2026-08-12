3e8c7d3743a7283821781eb03c26c6a7f8f766d2
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-12 18:07:49 +0530

    Time: 0 ms (100%), Space: 19.5 MB (48.55%) - LeetHub

---
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # prefixSum = 0 
+        # prefixSumMap = {}
+
+        # dummy = ListNode()
+        # dummy.next = head
+
+        # prefixSumMap[0] = dummy
+
+        # while head:
+        #     prefixSum += head.val
+
+        #     if prefixSum in prefixSumMap:
+        #         prefixSumMap[prefixSum].next = head.next
+        #         head = prefixSumMap[prefixSum]
+        #     else:
+        #         prefixSumMap[prefixSum] = head
+            
+        #     head = head.next
+    
+
+        # return dummy.next
+
+
+        prefixSum = 0 
+        prefixSumMap = {}
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        prefixSumMap[0] = dummy
+
+        while head:
+            prefixSum += head.val
+            prefixSumMap[prefixSum] = head
+            head = head.next
+
+        head = dummy
+        prefixSum = 0
+
+        while head:
+            prefixSum += head.val
+            head.next = prefixSumMap[prefixSum].next
+            head = head.next
+    
+        return dummy.next

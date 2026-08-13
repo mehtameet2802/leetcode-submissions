bb81db0d5849ca2eee153ba7e3c2ead0e946a8df
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-13 12:33:04 +0530

    Time: 4 ms (26.77%), Space: 19.7 MB (11.5%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -7,29 +7,60 @@
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
         
+        # def helper(root1, root2):
+        #     if not root1 and not root2:
+        #         return None
+            
+        #     if not root1:
+        #         new_node = TreeNode(root2.val)
+        #         new_node.left = helper(None, root2.left)
+        #         new_node.right = helper(None, root2.right)
+        #         return new_node
+            
+        #     if not root2:
+        #         new_node = TreeNode(root1.val)
+        #         new_node.left = helper(root1.left, None)
+        #         new_node.right = helper(root1.right, None)
+        #         return new_node
+
+
+        #     mergeSum = root1.val + root2.val
+
+        #     new_node = TreeNode(mergeSum)
+
+        #     new_node.left = helper(root1.left, root2.left)
+        #     new_node.right = helper(root1.right, root2.right)
+        
+        #     return new_node
+
+        # return helper(root1, root2)
+
+
         def helper(root1, root2):
             if not root1 and not root2:
                 return None
             
-            if not root1:
-                new_node = TreeNode(root2.val)
-                new_node.left = helper(None, root2.left)
-                new_node.right = helper(None, root2.right)
-                return new_node
+            mergeSum = 0
+            if root1:
+                mergeSum += root1.val
+
+            if root2:
+                mergeSum += root2.val
             
-            if not root2:
-                new_node = TreeNode(root1.val)
-                new_node.left = helper(root1.left, None)
-                new_node.right = helper(root1.right, None)
-                return new_node
-
-
-            mergeSum = root1.val + root2.val
-
             new_node = TreeNode(mergeSum)
 
-            new_node.left = helper(root1.left, root2.left)
-            new_node.right = helper(root1.right, root2.right)
+            if not root1:
+                left = helper(None, root2.left)
+                right = helper(None, root2.right)
+            elif not root2:
+                left = helper(root1.left, None)
+                right = helper(root1.right, None)
+            else:
+                left = helper(root1.left, root2.left)
+                right = helper(root1.right, root2.right)
+
+            new_node.left = left
+            new_node.right = right
         
             return new_node
 

33ead0459cc9ad0e8166a93e8066ec557bd95c7d
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-13 16:26:55 +0530

    Time: 16 ms (81.89%), Space: 23.6 MB (38.74%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.py
@@ -8,35 +8,56 @@ class Solution:
     def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         # Definition for a binary tree node.
 
+        # '''
+        # TC - O(N)
+        # SC - O(W)
+        # '''
+
+        # if not root:
+        #     return root
+
+        # queue = deque([])
+        # queue.append(root)
+        # level = 0
+
+        # while queue:
+
+        #     length = len(queue)
+
+        #     nums = [num.val for num in queue]
+        #     for i in range(length):
+        #         ele = queue.popleft()
+
+        #         if level % 2 == 1:
+        #             ele.val = nums[-1 - i] 
+
+        #         if ele.left:
+        #             queue.append(ele.left)
+                
+        #         if ele.right:
+        #             queue.append(ele.right)
+            
+        #     level += 1
+
+        # return root
+
         '''
         TC - O(N)
         SC - O(W)
         '''
 
         if not root:
-            return ans
+            return root
 
-        queue = deque([])
-        queue.append(root)
-        level = 0
-
-        while queue:
-
-            length = len(queue)
-
-            nums = [num.val for num in queue]
-            for i in range(length):
-                ele = queue.popleft()
-
-                if level % 2 == 1:
-                    ele.val = nums[-1 - i] 
-
-                if ele.left:
-                    queue.append(ele.left)
-                
-                if ele.right:
-                    queue.append(ele.right)
+        def helper(left, right, level):
+            if not left:
+                return
             
-            level += 1
-
+            if level % 2 == 1:
+                left.val, right.val = right.val, left.val
+            
+            helper(left.left, right.right, level + 1)
+            helper(left.right, right.left, level + 1)
+        
+        helper(root.left, root.right, 1)
         return root

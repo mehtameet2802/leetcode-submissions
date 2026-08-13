293e013055411d5f780940019da4fae840af3e00
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-13 11:24:29 +0530

    Time: 0 ms (100%), Space: 22.4 MB (60.22%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,6 +6,18 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        '''
+        Pattern:
+        - Binary Tree
+        - DFS
+        - Recursion
+
+        TC - O(N)
+        SC - O(H)
+
+        Balanced → O(log N)
+        Worst case → O(N)
+        '''
         
         def helper(root):
             if not root:

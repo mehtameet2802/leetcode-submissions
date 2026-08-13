b54ec3db14921e036cac1007f06a9c3c019203c7
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-13 15:20:45 +0530

    Time: 55 ms (58.98%), Space: 21.2 MB (5.27%) - LeetHub

---
+++ 0429-n-ary-tree-level-order-traversal/0429-n-ary-tree-level-order-traversal.py
@@ -0,0 +1,44 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def levelOrder(self, root: 'Node') -> List[List[int]]:
+        '''
+        Pattern:
+        - Binary Tree
+        - BFS
+        - Queue
+        - Level Order
+
+        TC - O(N)
+        SC - O(W)
+        '''
+        ans = []
+
+        if not root:
+            return ans
+
+        queue = deque([])
+        queue.append(root)
+
+        while queue:
+
+            length = len(queue)
+            cur = []
+
+            for i in range(length):
+                ele = queue.popleft()
+                
+                for child in ele.children:
+                    queue.append(child)
+                
+                cur.append(ele.val)
+            
+            ans.append(cur)
+
+        return ans

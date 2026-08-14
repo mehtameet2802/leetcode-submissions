986e67f5a220b967badfac5402d6eb419f470c81
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-14 20:24:38 +0530

    Time: 1 ms (31.85%), Space: 19.6 MB (62.06%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+
+        stack = [0]
+        visited = set()
+
+        while stack:
+            room = stack.pop()
+            visited.add(room)
+
+            for key in rooms[room]:
+                if key not in visited:
+                    stack.append(key)
+        
+        return True if len(visited) == len(rooms) else False

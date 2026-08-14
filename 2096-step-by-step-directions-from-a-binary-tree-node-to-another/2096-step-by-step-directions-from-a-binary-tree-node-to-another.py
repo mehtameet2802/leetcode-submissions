293a88463c81fe3066822b4148d5783ada142599
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-14 16:09:13 +0530

    Time: 104 ms (37.78%), Space: 74.3 MB (39.6%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -7,63 +7,102 @@
 class Solution:
     def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
         
-        def getLCA(node):
+        # def getLCA(node):
+        #     if not node:
+        #         return None
+            
+        #     if node.val == startValue or node.val == destValue:
+        #         return node
+            
+        #     left = getLCA(node.left)
+        #     right = getLCA(node.right)
+
+        #     if left and right:
+        #         return node
+            
+        #     if left:
+        #         return left
+            
+        #     return right
+
+
+        # def dfs(node, value):
+        #     if not node:
+        #         return "", False
+
+        #     if node.val == value:
+        #         return "", True
+            
+        #     d1, left = dfs(node.left, value)
+        #     d2, right = dfs(node.right, value)
+
+        #     if left:
+        #         state = "L"+d1
+        #         return state, True
+            
+        #     if right:
+        #         state = "R"+d2
+        #         return state, True
+            
+        #     return "", False
+            
+        
+        # lca = getLCA(root)
+
+        # ans = ""
+        # if lca.val == startValue:
+        #     ans, _ = dfs(lca, destValue)
+        
+        # elif lca.val == destValue:
+        #     ans, _ = dfs(lca, startValue)
+        #     ans = "U"*len(ans)
+
+        # else:
+        #     ans1, _ = dfs(lca, startValue)
+        #     ans2, _ = dfs(lca, destValue)
+
+        #     ans1 = "U"*len(ans1)
+        #     ans = ans1+ans2
+
+        # return ans
+
+
+        start_path = []
+        end_path = []
+        path = []
+
+        def helper(node):
+
             if not node:
-                return None
+                return
+
+            if node.val == startValue:
+                start_path.extend(path)
             
-            if node.val == startValue or node.val == destValue:
-                return node
+            if node.val == destValue:
+                end_path.extend(path)
+
+            if start_path and end_path:
+                return
             
-            left = getLCA(node.left)
-            right = getLCA(node.right)
+            path.append("L")
+            helper(node.left)
+            path.pop()
 
-            if left and right:
-                return node
-            
-            if left:
-                return left
-            
-            return right
+            if start_path and end_path:
+                return
+
+            path.append("R")
+            helper(node.right)
+            path.pop()
+
+        helper(root)
+
+        i = 0
+        while i<len(start_path) and i<len(end_path) and start_path[i] == end_path[i]:
+            i += 1
+
+        return "U"*(len(start_path) - i) + "".join(end_path[i:])
 
 
-        def dfs(node, value):
-            if not node:
-                return "", False
-
-            if node.val == value:
-                return "", True
-            
-            d1, left = dfs(node.left, value)
-            d2, right = dfs(node.right, value)
-
-            if left:
-                state = "L"+d1
-                return state, True
-            
-            if right:
-                state = "R"+d2
-                return state, True
-            
-            return "", False
-            
-        
-        lca = getLCA(root)
-
-        ans = ""
-        if lca.val == startValue:
-            ans, _ = dfs(lca, destValue)
-        
-        elif lca.val == destValue:
-            ans, _ = dfs(lca, startValue)
-            ans = "U"*len(ans)
-
-        else:
-            ans1, _ = dfs(lca, startValue)
-            ans2, _ = dfs(lca, destValue)
-
-            ans1 = "U"*len(ans1)
-            ans = ans1+ans2
-
-        return ans
-        
         

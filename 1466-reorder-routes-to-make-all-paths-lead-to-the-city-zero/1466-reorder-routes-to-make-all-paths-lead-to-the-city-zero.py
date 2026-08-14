43a577bdfbadb9798983f1f9c63a7d995d526989
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-14 18:47:23 +0530

    Time: 188 ms (20.33%), Space: 48 MB (69.43%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,25 +1,47 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        visited = set()
+        # visited = set()
+        # graph = defaultdict(list)
+        
+        # for u,v in connections:
+        #     graph[u].append((v,1))
+        #     graph[v].append((u,0))
+
+
+        # def helper(node):
+        #     visited.add(node)
+
+        #     count = 0
+        #     for nei, cost in graph[node]:
+        #         if nei in visited:
+        #             continue
+                
+        #         count += cost
+        #         count += helper(nei)
+            
+        #     return count
+        
+        # return helper(0)
+
+
+        stack = [0]
+        visited = {0}
+        cnt = 0
         graph = defaultdict(list)
         
         for u,v in connections:
             graph[u].append((v,1))
             graph[v].append((u,0))
 
-
-        def helper(node):
+        while stack:
+            node = stack.pop()
             visited.add(node)
-
             count = 0
             for nei, cost in graph[node]:
                 if nei in visited:
                     continue
                 
-                count += cost
-                count += helper(nei)
-            
-            return count
-        
-        return helper(0)
-        
+                cnt += cost
+                stack.append(nei)
+
+        return cnt

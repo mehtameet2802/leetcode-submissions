84bf8e252e6d68431ad9978d340d513a3fb7ad4c
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-14 12:33:30 +0530

    Time: 0 ms (100%), Space: 19.4 MB (90.7%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -6,28 +6,43 @@
 #         self.right = right
 class Solution:
     def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
-        ans = 0
+        # ans = 0
 
-        path = []
+        # path = []
 
-        def helper(node):
-            nonlocal ans
+        # def helper(node):
+        #     nonlocal ans
             
-            path.append(str(node.val))
-            if not node.left and not node.right:
-                value = "".join(path)
-                ans += int(value,2)
-                path.pop()
-                return
+        #     path.append(str(node.val))
+        #     if not node.left and not node.right:
+        #         value = "".join(path)
+        #         ans += int(value,2)
+        #         path.pop()
+        #         return
             
-            if node.left:
-                helper(node.left)
+        #     if node.left:
+        #         helper(node.left)
             
-            if node.right:
-                helper(node.right)
+        #     if node.right:
+        #         helper(node.right)
             
-            path.pop()
+        #     path.pop()
         
-        helper(root)
-        return ans
+        # helper(root)
+        # return ans
+
+        def helper(node, cur):
+            if not node:
+                return 0
+
+            cur = cur * 2 + node.val
+            
+            if not node.left and not node.right:
+                return cur
+            
+            return helper(node.left, cur) + helper(node.right, cur)
+        
+        return helper(root, 0)
+
+
 

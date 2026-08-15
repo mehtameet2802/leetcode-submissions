d2d08e0d995fec0d6a388ad084d7d30cf71a879f
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-15 15:21:56 +0530

    Time: 3044 ms (23.47%), Space: 188.6 MB (8.16%) - LeetHub

---
+++ 2059-minimum-operations-to-convert-number/2059-minimum-operations-to-convert-number.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minimumOperations(self, nums: List[int], start: int, goal: int) -> int:
+        queue = deque([(start,0)])
+        visited = {start}
+        
+        while queue:
+            val, cnt = queue.popleft()
+
+            if val == goal:
+                return cnt
+
+            if val>1000 or val<0:
+                continue
+
+            for num in nums:
+
+                val1 = val + num
+                val2 = val - num
+                val3 = val ^ num
+
+                if val1 not in visited:
+                    queue.append((val1, cnt + 1))
+                    visited.add(val1)
+                
+                if val2 not in visited:
+                    queue.append((val2, cnt + 1))
+                    visited.add(val2)
+                
+                if val3 not in visited:
+                    queue.append((val3, cnt + 1))
+                    visited.add(val3)
+        
+        return -1
+
+            

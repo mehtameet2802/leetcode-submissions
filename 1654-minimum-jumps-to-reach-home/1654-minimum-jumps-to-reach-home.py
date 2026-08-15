719d9d2edf0b1d029050afef31afa0a713cb3518
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-15 15:11:24 +0530

    Time: 32 ms (34.9%), Space: 21 MB (26.04%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minimumJumps(self, forbidden: List[int], a: int, b: int, x: int) -> int:
+
+        queue = deque([(0,"S",0)])
+        visited = {(0,"F")}
+        forbidden = set(forbidden)
+
+        if 0 in forbidden:
+            return -1
+
+        while queue:
+            pos, jump, jumps = queue.popleft()
+
+            if pos == x:
+                return jumps
+
+            nf = pos + a
+
+            if nf<=6000 and nf not in forbidden and (nf,"F") not in visited:
+                visited.add((nf,"F"))
+                queue.append((nf,"F",jumps+1))
+
+            nb = pos - b
+
+            if jump != "B" and nb>=0 and nb not in forbidden and (nb,"B") not in visited:
+                visited.add((nb,"B"))
+                queue.append((nb,"B",jumps+1))
+                
+        return -1 

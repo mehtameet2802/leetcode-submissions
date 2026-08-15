730c072e90a2f255086c23c841ea63ee94d460bd
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-15 12:26:16 +0530

    Time: 119 ms (91.43%), Space: 21 MB (29.18%) - LeetHub

---
+++ 0690-employee-importance/0690-employee-importance.py
@@ -0,0 +1,29 @@
+"""
+# Definition for Employee.
+class Employee:
+    def __init__(self, id: int, importance: int, subordinates: List[int]):
+        self.id = id
+        self.importance = importance
+        self.subordinates = subordinates
+"""
+
+class Solution:
+    def getImportance(self, employees: List['Employee'], id: int) -> int:
+        graph = {}
+
+        for employee in employees:
+            graph[employee.id] = employee
+        
+        def helper(employee):
+            
+            total = employee.importance
+            for sub in employee.subordinates:
+                total += helper(graph[sub])
+            
+            return total
+        
+        return helper(graph[id])
+
+        
+        
+        

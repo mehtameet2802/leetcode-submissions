e23b453726ae40303a6d88638ab73bae01a85764
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-16 19:55:23 +0530

    Time: 30 ms (5.01%), Space: 20.3 MB (7.55%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -0,0 +1,32 @@
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        
+        day = {
+            0:1,
+            1:7,
+            2:30
+        }
+        
+        dp = {}
+
+        def helper(i, reach):
+
+            if (i,reach) in dp:
+                return dp[(i,reach)]
+
+            if i >= len(days):
+                return 0
+            
+            ans = float('inf')
+
+            if days[i]<=reach:
+                return helper(i+1, reach)
+
+            for j in range(len(costs)):
+                ans = min(ans, costs[j] + helper(i+1,days[i] + day[j]-1))
+            
+            dp[(i,reach)] = ans
+            return ans
+
+        return helper(0, 0)
+

ce27f4005cb767ed1585c5d101330a22fc5621b9
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-16 17:16:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (61.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -23,22 +23,67 @@ class Solution:
         # return helper(0)
 
 
-        '''
-        Pattern - DP
+        # '''
+        # Pattern - DP
 
-        TC - O(n)
-        SC - O(1)
-        '''
+        # TC - O(n)
+        # SC - O(1)
+        # '''
+
+        # n = len(nums)
+        # if n<3:
+        #     return max(nums)
+        
+        # last = nums[n-1]
+        # prev1 = max(nums[n-1],nums[n-2])
+
+        # for i in range(n-3,-1,-1):
+        #     cur = max(nums[i]+last,prev1)
+        #     last, prev1 = prev1, cur
+        
+        # return prev1
+
+
+
+
+
+
+
+
+
+
+        if len(nums) < 2:
+            return max(nums)
 
         n = len(nums)
-        if n<3:
-            return max(nums)
-        
-        last = nums[n-1]
-        prev1 = max(nums[n-1],nums[n-2])
+        dp = [0]*len(nums)
+        dp[n-1] = nums[n-1]
+        dp[n-2] = max(nums[n-2],nums[n-1])
 
         for i in range(n-3,-1,-1):
-            cur = max(nums[i]+last,prev1)
-            last, prev1 = prev1, cur
+            dp[i] = max(dp[i+1],dp[i+2]+nums[i])
         
-        return prev1
+        return max(dp[0], dp[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

ea1390b0a72c58ffc8e7f7136df344128659bb10
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-16 12:12:54 +0530

    Time: 8 ms (25.22%), Space: 19.9 MB (54.22%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        path = []
+        used = set()
+
+        def helper():
+            if len(path) == len(nums):
+                ans.append(path.copy())
+                return
+            
+            for i in range(len(nums)):
+
+                if i in used:
+                    continue
+                
+                if i>0 and nums[i] == nums[i-1] and i-1 not in used:
+                    continue
+
+                used.add(i)
+                path.append(nums[i])
+                helper()
+                path.pop()
+                used.remove(i)
+        
+        helper()
+        return ans

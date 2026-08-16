d3e71d7b454a3a7b9661faf74f2e785b2ae88125
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-16 17:30:48 +0530

    Time: 0 ms (100%), Space: 19.2 MB (92.85%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -34,60 +34,28 @@ class Solution:
         # return max(helper(0,True), helper(1, False))
 
 
-        # '''
-        # Pattern - 
+        '''
+        Pattern - 
 
-        # TC - O(n)
-        # SC - O(1)
-        # '''
+        TC - O(n)
+        SC - O(1)
+        '''
 
-        # n = len(nums)
+        n = len(nums)
 
-        # if n == 1:
-        #     return nums[0]
-        
-        # def robber(s,e):
-
-        #     last = nums[e]
-        #     prev1 = max(nums[e-1],last)
-
-        #     for i in range(e-2,s-1,-1):
-        #         cur = max(nums[i]+last,prev1)
-        #         last, prev1 = prev1, cur
-            
-        #     return prev1
-        
-        # return max(robber(0,n-2), robber(1, n-1))
-
-
-
-
-
-
-
-
-
-
-        if len(nums) == 1:
+        if n == 1:
             return nums[0]
-
-        def robber(start,end):
-
-            last1 = nums[end]
-            last2 = max(nums[end-1],nums[end])
-
-            for i in range(end-2,start-1,-1):
-                cur = max(last2, last1+nums[i])
-                last2, last1 = cur, last2
-            
-            return last2
         
-        return max(robber(0,len(nums)-2),robber(1,len(nums)-1))
+        def robber(s,e):
 
+            last = nums[e]
+            prev1 = max(nums[e-1],last)
 
-
-
-
-
-
+            for i in range(e-2,s-1,-1):
+                cur = max(nums[i]+last,prev1)
+                last, prev1 = prev1, cur
+            
+            return prev1
+        
+        return max(robber(0,n-2), robber(1, n-1))
 

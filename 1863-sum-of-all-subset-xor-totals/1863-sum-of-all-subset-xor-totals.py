93c0e1864de39984afc2bd5710ffd6892cb9a1ce
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-16 10:49:08 +0530

    Time: 25 ms (24.6%), Space: 19.3 MB (59.81%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -0,0 +1,23 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        ans = 0
+        values = []
+
+        def xor():
+            val = 0
+            for num in values:
+                val = val ^ num
+            
+            return val
+        
+        def subsets(i):
+            nonlocal ans
+            ans += xor()
+            
+            for j in range(i, len(nums)):
+                values.append(nums[j])
+                subsets(j+1)
+                values.pop()
+            
+        subsets(0)
+        return ans

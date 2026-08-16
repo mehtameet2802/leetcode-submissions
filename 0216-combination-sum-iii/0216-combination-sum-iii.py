e5e9f28f439cbc64d584f067b1dbddeb7a2f02fd
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-16 15:11:29 +0530

    Time: 0 ms (100%), Space: 19.2 MB (72.4%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+        path = []
+
+        def helper(i, cur):
+            if len(path) == k and cur == n:
+                ans.append(path.copy())
+                return
+            
+            if len(path) > k  or cur > n:
+                return
+            
+            for j in range(i,10):
+                path.append(j)
+                helper(j+1, cur + j)
+                path.pop()
+            
+        helper(1,0)
+        return ans

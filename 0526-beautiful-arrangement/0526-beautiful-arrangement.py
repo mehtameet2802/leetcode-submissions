b8949555311d679d133caf29634e73274ff01900
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-16 12:55:38 +0530

    Time: 864 ms (20.82%), Space: 19.5 MB (28.48%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -21,3 +21,30 @@ class Solution:
         
         helper(1)
         return ans
+
+
+        # ans = 0
+        # path = []
+        # seen = set()
+
+        # def helper():
+        #     nonlocal ans
+
+        #     if len(path) == n:
+        #         ans += 1
+        #         return
+            
+        #     for i in range(1,n+1):
+        #         if i in seen:
+        #             continue
+                
+        #         idx = len(path) + 1
+        #         if i % idx == 0 or idx % i == 0:
+        #             seen.add(i)
+        #             path.append(i)
+        #             helper()
+        #             path.pop()
+        #             seen.remove(i)
+        
+        # helper()
+        # return ans

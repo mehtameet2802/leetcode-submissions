bc6b5a507151e3c542c06770ab9d4c5f87514cca
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-16 14:09:57 +0530

    Time: 1073 ms (83.68%), Space: 20.2 MB (56.74%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -0,0 +1,19 @@
+class Solution:
+    def beautifulSubsets(self, nums: List[int], k: int) -> int:
+        ans = 0
+        path = []
+
+        def helper(i):
+            nonlocal ans
+
+            ans += 1
+
+            for j in range(i, len(nums)):
+                # Take nums[i] if valid
+                if nums[j] - k not in path and nums[j] + k not in path:
+                    path.append(nums[j])
+                    helper(j + 1)
+                    path.pop()
+
+        helper(0)
+        return ans-1

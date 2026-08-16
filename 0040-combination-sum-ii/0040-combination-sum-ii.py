4123ee6d4ffd43af981127bf602dc2d8bcb98531
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-16 14:55:10 +0530

    Time: 19 ms (50.96%), Space: 19.4 MB (73.73%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
         path = []
-        seen = set()
         candidates.sort()
 
         def helper(i, cur):
@@ -17,10 +16,8 @@ class Solution:
                 if j>i and candidates[j] == candidates[j-1]:
                     continue
                 
-                # seen.add(candidates[j])
                 path.append(candidates[j])
                 helper(j+1, cur+candidates[j])
-                # seen.remove(candidates[j])
                 path.pop()
             
         helper(0,0)

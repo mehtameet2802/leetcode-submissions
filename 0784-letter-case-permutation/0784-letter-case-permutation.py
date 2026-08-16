0a9e7288f70b543e4367c129463d085eca034d0b
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-16 11:16:26 +0530

    Time: 463 ms (5.05%), Space: 20.6 MB (55.89%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        path = []
+        ans = []
+
+
+        def helper(i):
+            if len(path) == len(s):
+                ans.append("".join(path))
+                return
+            
+            for j in range(i,len(s)):
+                if s[j].isalpha():
+                    path.append(s[j].lower())
+                    helper(j+1)
+                    path.pop()
+
+                    path.append(s[j].upper())
+                    helper(j+1)
+                    path.pop()
+                else:
+                    path.append(s[j])
+                    helper(j+1)
+                    path.pop()
+            
+        helper(0)
+        return ans

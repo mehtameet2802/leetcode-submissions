84ecd11e2e561cae7aad2c02642c0320ea4af7bb
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-17 19:26:17 +0530

    Time: 3 ms (72.69%), Space: 19.6 MB (13.99%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,22 +1,43 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         
+        # wordDict = set(wordDict)
+        # dp = {}
+
+        # def helper(i,length):
+        #     if (i,length) in dp:
+        #         return dp[(i,length)]
+            
+        #     if i == len(s):
+        #         return length == 0
+            
+        #     ans = False
+        #     if s[i-length:i+1] in wordDict:
+        #         ans = helper(i+1,0)
+
+        #     ans = ans or helper(i+1, length + 1)
+        #     dp[(i,length)] = ans
+        #     return ans
+
+        # return helper(0,0)  
+
         wordDict = set(wordDict)
         dp = {}
 
-        def helper(i,length):
-            if (i,length) in dp:
-                return dp[(i,length)]
+        def helper(i):
+            if i in dp:
+                return dp[i]
             
             if i == len(s):
-                return length == 0
-            
-            ans = False
-            if s[i-length:i+1] in wordDict:
-                ans = helper(i+1,0)
+                return True
 
-            ans = ans or helper(i+1, length + 1)
-            dp[(i,length)] = ans
-            return ans
+            for j in range(i+1, len(s)+1):
+                if s[i:j] in wordDict:
+                    if helper(j):
+                        dp[i] = True
+                        return True
 
-        return helper(0,0)  
+            dp[i] = False
+            return False
+
+        return helper(0)  

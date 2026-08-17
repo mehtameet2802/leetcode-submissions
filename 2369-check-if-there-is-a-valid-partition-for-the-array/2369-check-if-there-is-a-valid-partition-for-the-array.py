e89af08ced0ead65628f31b8d080f8012dab0cd7
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-17 20:58:48 +0530

    Time: 123 ms (79.65%), Space: 80.3 MB (33.19%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,22 +1,43 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
         
-        def check(arr):
-            arr1 = set(arr)
+        # def check(arr):
+        #     arr1 = set(arr)
 
-            if len(arr) != 2 and len(arr)!=3:
-                return False
+        #     if len(arr) != 2 and len(arr)!=3:
+        #         return False
 
-            if len(arr1) == 1:
-                return True
+        #     if len(arr1) == 1:
+        #         return True
             
-            if len(arr1) == 3:
-                for i in range(2):
-                    if arr[i+1] - arr[i] != 1:
-                        return False
-                return True
+        #     if len(arr1) == 3:
+        #         for i in range(2):
+        #             if arr[i+1] - arr[i] != 1:
+        #                 return False
+        #         return True
         
-            return False
+        #     return False
+        
+        # dp = {}
+
+        # def helper(i):
+        #     if i in dp:
+        #         return dp[i]
+
+        #     if i>=len(nums):
+        #         return True
+            
+        #     for j in range(i+2,min(i+4,len(nums)+1)):
+        #         if check(nums[i:j]):
+        #             if helper(j):
+        #                 dp[i] = True
+        #                 return True
+            
+        #     dp[i] = False
+        #     return False
+        
+        # return helper(0)
+
         
         dp = {}
 
@@ -26,12 +47,18 @@ class Solution:
 
             if i>=len(nums):
                 return True
+
+            if (i+1 < len(nums) and nums[i] == nums[i+1] and helper(i+2)):
+                dp[i] = True
+                return True
+
+            if (i+2<len(nums) and nums[i] == nums[i+1] == nums[i+2] and helper(i+3)):
+                dp[i] = True
+                return True
             
-            for j in range(i+2,min(i+4,len(nums)+1)):
-                if check(nums[i:j]):
-                    if helper(j):
-                        dp[i] = True
-                        return True
+            if (i+2<len(nums) and nums[i+1] == nums[i]+1 and nums[i+2] == nums[i]+2 and helper(i+3)):
+                dp[i] = True
+                return True
             
             dp[i] = False
             return False

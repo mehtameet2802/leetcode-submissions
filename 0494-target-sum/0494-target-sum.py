e9b66a268179c432b9fbee8d48036443ca50735c
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-18 13:01:05 +0530

    Time: 19 ms (89.34%), Space: 19.3 MB (90.87%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,16 +1,36 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         
-        dp = {}
+        # dp = {}
 
-        def helper(i, cur):
-            if (i,cur) in dp:
-                return dp[(i,cur)]
+        # def helper(i, cur):
+        #     if (i,cur) in dp:
+        #         return dp[(i,cur)]
 
-            if i == len(nums):
-                return 1 if cur == target else 0
+        #     if i == len(nums):
+        #         return 1 if cur == target else 0
             
-            dp[(i,cur)] = helper(i+1,cur-nums[i]) + helper(i+1, cur+nums[i])
-            return dp[(i,cur)]
+        #     dp[(i,cur)] = helper(i+1,cur-nums[i]) + helper(i+1, cur+nums[i])
+        #     return dp[(i,cur)]
 
-        return helper(0,0)
+        # return helper(0,0)
+
+
+        total = sum(nums)
+
+        if abs(target) > total:
+            return 0
+
+        if (total + target) % 2:
+            return 0
+
+        subset_target = (total + target) // 2
+
+        dp = [0] * (subset_target + 1)
+        dp[0] = 1
+
+        for num in nums:
+            for cur in range(subset_target, num - 1, -1):
+                dp[cur] += dp[cur - num]
+
+        return dp[subset_target]

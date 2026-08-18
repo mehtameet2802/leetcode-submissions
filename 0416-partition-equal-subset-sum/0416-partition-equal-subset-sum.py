11cccfb1818854b9b181f12b07249ba2d8f35794
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-18 12:12:30 +0530

    Time: 641 ms (68.22%), Space: 19.2 MB (84.08%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,30 +1,49 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        # total = sum(nums)
+
+        # if total % 2 :
+        #     return False
+
+        # target = total // 2
+        # dp = {}
+
+        # def helper(i,target):
+        #     if target == 0:
+        #         return True
+            
+        #     if target<0:
+        #         return False
+            
+        #     if i>=len(nums):
+        #         return False
+
+        #     if (i,target) in dp:
+        #         return dp[(i,target)]
+            
+        #     ans = helper(i+1, target - nums[i]) or helper(i+1, target)
+        #     dp[(i,target)] = ans
+        #     return ans
+            
+        
+        # return helper(0, target)
+
+
         total = sum(nums)
 
         if total % 2 :
             return False
 
         target = total // 2
-        dp = {}
+        dp = [False] * (target + 1)
+        dp[0] = True
 
-        def helper(i,target):
-            if target == 0:
-                return True
-            
-            if target<0:
-                return False
-            
-            if i>=len(nums):
-                return False
+        for num in nums:
 
-            if (i,target) in dp:
-                return dp[(i,target)]
+            for t in range(target, num-1, -1):
+                dp[t] = dp[t] or dp[t-num]
             
-            ans = helper(i+1, target - nums[i]) or helper(i+1, target)
-            dp[(i,target)] = ans
-            return ans
+        return dp[target]
             
         
-        return helper(0, target)
-            
+        

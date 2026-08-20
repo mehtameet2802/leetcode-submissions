5d2d025e2525d56bfa634f06be0122bc78c11788
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-21 02:08:40 +0530

    Time: 216 ms (63.46%), Space: 33.6 MB (80.13%) - LeetHub

--- 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.py
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.py
@@ -2,15 +2,16 @@ class Solution:
     def maxSumMinProduct(self, nums: List[int]) -> int:
         n = len(nums)
 
-        prefix = [0]*(n+1)
+        prefix = [0]*n
+        prefix[0] = nums[0]
 
-        for i in range(n):
-            prefix[i+1] = prefix[i] + nums[i]
+        for i in range(1,n):
+            prefix[i] = prefix[i-1] + nums[i]
         
         stack = []
         ans = 0
 
-        for i in range(n+1):
+        for i in range(n):
             curr = nums[i] if i < n else 0
 
             while stack and nums[stack[-1]] >= curr:
@@ -20,10 +21,26 @@ class Solution:
                 left = stack[-1] if stack else -1
                 right = i
 
-                total = prefix[right] - prefix[left+1]
+                if left == -1:
+                    total = prefix[right-1]
+                else:
+                    total = prefix[right-1] - prefix[left]
 
                 ans = max(ans, total*nums[j])
 
-            if i < n:
-                stack.append(i)
+            stack.append(i)
+
+        while stack:
+            j = stack.pop()
+            left = stack[-1] if stack else -1
+            right = n
+
+            if left == -1:
+                    total = prefix[right-1]
+            else:
+                total = prefix[right-1] - prefix[left]
+
+            ans = max(ans, total*nums[j])
+
+        
         return ans % (10**9 + 7)

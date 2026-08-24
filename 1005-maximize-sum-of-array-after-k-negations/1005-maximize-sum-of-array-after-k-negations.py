ccde292d81082780fd3093942753bd5728fd17e4
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-24 11:52:48 +0530

    Time: 0 ms (100%), Space: 19.3 MB (38.35%) - LeetHub

--- 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
+++ 1005-maximize-sum-of-array-after-k-negations/1005-maximize-sum-of-array-after-k-negations.py
@@ -1,31 +1,56 @@
 class Solution:
     def largestSumAfterKNegations(self, nums: List[int], k: int) -> int:
-        neg = []
-        pos = []
+        # '''
+        # TC - O(n logn)
+        # SC - O(n)
+        # '''
 
-        for num in nums:
-            if num < 0:
-                neg.append(num)
-            else:
-                pos.append(num)
+        # neg = []
+        # pos = []
 
-        neg.sort()
+        # for num in nums:
+        #     if num < 0:
+        #         neg.append(num)
+        #     else:
+        #         pos.append(num)
+
+        # neg.sort()
         
         
-        i = 0
-        while k>0 and i<len(neg):
-            neg[i] = -1*neg[i]
-            i += 1
-            k -= 1
+        # i = 0
+        # while k>0 and i<len(neg):
+        #     neg[i] = -1*neg[i]
+        #     i += 1
+        #     k -= 1
 
-        if k>0:
-            pos.extend(neg)
-            pos.sort()
-            k = k%2
+        # if k>0:
+        #     pos.extend(neg)
+        #     pos.sort()
+        #     k = k%2
             
-            if k:
-                pos[0] = -1*pos[0]
+        #     if k:
+        #         pos[0] = -1*pos[0]
 
-            return sum(pos)
+        #     return sum(pos)
         
-        return sum(neg) + sum(pos)
+        # return sum(neg) + sum(pos)
+
+
+        '''
+        TC - O(n logn)
+        SC - O(n)
+        '''
+        nums.sort()
+            
+        i = 0
+        while k>0 and i<len(nums) and nums[i]<0:
+            nums[i] = nums[i]*-1
+            i+=1
+            k-=1
+
+        k = k%2
+        if k>0:
+            idx = nums.index(min(nums))
+            nums[idx] = -1*nums[idx]
+        
+        return sum(nums)

e33f6207831d7824bcb64fce979a8ef4d95323d3
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-08-24 11:18:33 +0530

    Time: 17 ms (7.13%), Space: 19.9 MB (14.75%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        min_heap = []
+        max_heap = []
+
+        def balance():
+            if abs(len(min_heap)-len(max_heap)) > 1:
+                if len(min_heap) > len(max_heap):
+                    heapq.heappush(max_heap, -heapq.heappop(min_heap))
+                else:
+                    heapq.heappush(min_heap, -heapq.heappop(max_heap))
+
+
+        def push(nums):
+            for num in nums:
+                if not max_heap:
+                    heapq.heappush(max_heap, -num)
+                elif -max_heap[0] >= num:
+                    heapq.heappush(max_heap, -num)
+                else:
+                    heapq.heappush(min_heap, num)
+                
+                balance()
+
+        push(nums1)
+        push(nums2)
+
+        print(min_heap)
+        print(max_heap)
+        
+        if len(min_heap) == len(max_heap):
+            return (min_heap[0] - max_heap[0])/2
+        elif len(min_heap) > len(max_heap):
+            return min_heap[0]
+        else:
+            return -max_heap[0]

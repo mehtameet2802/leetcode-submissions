20b8e291df97310211f530c3b5891512e9131681
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-03 15:08:07 +0530

    Time: 21 ms (57.81%), Space: 25.6 MB (43.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,23 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-
         '''
-        Pattern - Prefix Product + Suffix Product
+        TC - O(n)
+        SC - O(n)
 
-        TC - O(N)
-        SC - O(1) auxiliary
-             O(N) including output
+        First drew the test case and tried solving the problme on excalidraw by iterating
+        Found the solution but still during writing had to think a bit and go back and forth on ecalidraw during writing of suffix part (it seems I started writing code early)
+
+        then wrote code and 1 test case failed, found that in prefix calculation had started from 2 instead of 1, had to draw some iteration of failed test case on excalidraw to identify the root cause and then resolved it, also had to use print statement to print prefix_mul
         '''
 
-        n = len(nums)
-        pre_prod = [1]*n
+        prefix_arr = [1]*len(nums)
 
-        for i in range(1,n):
-            pre_prod[i] = pre_prod[i-1]*nums[i-1]
+        for idx in range(1,len(nums)):
+            prefix_arr[idx] = prefix_arr[idx-1] * nums[idx-1]
 
-        suffix = 1
-        for i in range(n-2,-1,-1):
-            suffix = suffix*nums[i+1]
-            pre_prod[i] *= suffix
+        suffix_mul = nums[-1]
+
+        for idx in range(len(nums)-2,-1,-1):
+            prefix_arr[idx] = prefix_arr[idx] * suffix_mul
+            suffix_mul = suffix_mul * nums[idx]
         
-        return pre_prod
+        return prefix_arr

2d58f20443f8acfc5bd06d66bc52c3252b1bf2a9
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-03 18:58:42 +0530

    Time: 3 ms (94.55%), Space: 21.1 MB (49.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,19 +1,30 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l = 0
-        r = len(height)-1
-        l_max = 0
-        r_max = 0
+        '''
+        left pointer on left start
+        right pointer on right start
+        the pointer wiht lower height moves forward
+        at each index, it is checked if left is smaller or right is smaller
+        at each array index max of left and max of right is calculated
+        water is added which is max of current min - current min
+        return ans
+        '''
+
+        left = 0
+        right = len(height) - 1
+        max_l = 0
+        max_r = 0
         ans = 0
 
-        while l <= r:
-            if height[l] <= height[r]:
-                l_max = max(l_max, height[l])
-                ans += l_max - height[l]
-                l+=1
+        while left < right:
+            if height[left] < height[right]:
+                max_l = max(height[left], max_l)
+                ans += max_l - height[left]
+                left += 1
             else:
-                r_max = max(r_max, height[r])
-                ans += r_max - height[r]
-                r -= 1 
+                max_r = max(height[right], max_r)
+                ans += max_r - height[right]
+                right -= 1
         
         return ans
+            

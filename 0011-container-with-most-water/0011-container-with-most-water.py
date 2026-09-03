aa0b5aa792588921b41be163eb9434de01b2f2ef
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-03 17:04:53 +0530

    Time: 44 ms (94.71%), Space: 29.9 MB (5.52%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,18 +1,37 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        ans = 0
+        '''
+        1. What is the required output? - max area
+        2. Which constraints eliminate brute force? - we take 2 pointers and the pointer with lower height is moved inwards, until 2 pointers cross each other
+        3. Draw one normal and one dangerous example.
+        4. State the brute-force approach. - we do 2 for loop and check all pairs of indexes to determine the max area
+        5. State what each variable/data structure represents. - left is the array elements from left side, right is array elements from right, area is the max area, cur_area is the area for cur combination of left and right 
+        6. Write the invariant. 
+        7. Fix the loop boundaries. left < right
+        8. Dry-run one iteration.
 
-        l = 0
-        r = len(height)-1
 
-        while l<=r:
-            h = min(height[l],height[r])
-            a = (r-l)*h
-            ans = max(ans,a)
+        The question is seen multiple times and solved multiples time hence in memor, but ufcourse used excalidraw and wrote above answers
 
-            if height[l]<height[r]:
-                l+=1
+        TC - O(n)
+        SC - O(1)
+        '''
+
+        left = 0
+        right = len(height) - 1
+        area = 0
+
+        while left < right:
+            width = right - left
+            if height[left] < height[right]:
+                cur_area = height[left] * width
+                left += 1
             else:
-                r-=1
+                cur_area = height[right] * width 
+                right -= 1
+            
+            area = max(area, cur_area)
         
-        return ans
+        return area
+                
+

6e3907f1c73dc560fc1f1fd0f9a333fbacc9f3a8
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-03 15:25:49 +0530

    Time: 35 ms (46.4%), Space: 21.8 MB (56.41%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -2,18 +2,39 @@ from collections import defaultdict
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        seen = defaultdict(int)
+        '''
+        TC - O(n)
+        SC - O(n)
 
-        prefix = 0
-        ans = 0
 
-        seen[0] = 1
+        Drew the test cases on the excalidraw
+        when iterated throught testcase 1 - easily found that will require an array and dict, while writing the text realised only need dict to store freq of prefix sum
 
-        for num in nums:
-            prefix += num
-            
-            ans += seen[prefix-k]
-            
-            seen[prefix] += 1
+        when iterated through the testcase 2, found hidden condition of comapring current val in nums with k, if they match then ans_count increases by 1
+
+        during iteration we subtract k from prefix sum, and check if the val is in dict, if exists then we add its freq to ans, as that is the number of times the sub array sum equal to k, from that i to cur num (j), creates sub array with sum as k
+
+        if would have not checked the second test must have missed the comaprision of k == num in iteration
+
+        dict will have default value 0 and a value 0 with freq 1 to represent array startign from 0
+
+        '''
+
+        prefix_sum = [0]*len(nums)
+        freq_dict = defaultdict(int)
+        freq_dict[0] = 1
+        sub_arr_cnt = 0
+
+        prefix_sum = 0
+
+        for cur_ele in nums:
+
+            prefix_sum += cur_ele
+
+            diff = prefix_sum - k
+            if diff in freq_dict:
+                sub_arr_cnt += freq_dict[diff]
+
+            freq_dict[prefix_sum] += 1
         
-        return ans
+        return sub_arr_cnt

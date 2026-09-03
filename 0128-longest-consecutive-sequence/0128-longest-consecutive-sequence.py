fb6bbf6d722b239a67a2e89d9a01e4714a4109fe
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-03 13:57:34 +0530

    Time: 51 ms (53.95%), Space: 36.6 MB (66.83%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,32 +1,43 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        '''
+        I will create a set of all the elements
 
-        """
-        Pattern - Use space to reduce time
+        Then iterate over the elements and for each element check if the 
+        cur_ele-1 exist or not.
 
-        TC - O(n)
+        if cur_ele-1 exist then continue
+        else consider that the start point and start iterating by +1 until elemnts exist
+
+        This will provide me the count of ans, and I will update the ans with max
+
+        I could recollect this from memory as have solve it multiple times
+
+
+        Wrote first solution without considered in 5 min and submitted passed 81 cases and got TLE, thought 7 mins more and added considered set along with its condition to prevent duplicate runs from same starb points
+
+        TC - O(n) - because very less elements for which I will be able to iterate by +1 until n, most will break
         SC - O(n)
-        """
+        '''
 
-        seen = set()
-        
-        for num in nums:
-            seen.add(num)
-        
-        ans = 0
-        for num in seen:
-            if num-1 in seen:
+        seen = set(nums)
+        considered = set()
+        seq_len = 0
+
+        for cur_ele in nums:
+            if cur_ele - 1 in seen:
                 continue
             
-            next = num+1
-            length = 1
-            while next in seen:
-                next = next+1
-                length += 1
+            if cur_ele in considered:
+                continue
+
+            considered.add(cur_ele)
+            next_ele = cur_ele + 1
+            cur_seq_len = 1
+            while next_ele in seen:
+                cur_seq_len += 1
+                next_ele += 1
             
-            ans = max(ans,length)
-        
-        return ans
+            seq_len = max(seq_len, cur_seq_len)
 
-
-        
+        return seq_len

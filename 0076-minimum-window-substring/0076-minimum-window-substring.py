fc86f470392ad481bb0b2685ac497054e63eb79d
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-05 19:33:41 +0530

    Time: 63 ms (73.71%), Space: 19.8 MB (37.71%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,22 +2,40 @@ class Solution:
     def minWindow(self, s: str, t: str) -> str:
         counter_t = Counter(t)
         counter_s = defaultdict(int)
-        ans = s
-
-        if len(s) < len(t) or Counter(s) & counter_t != counter_t:
-            return ""
+    
+        # if len(s) < len(t) or Counter(s) & counter_t != counter_t:
+        #     return ""
 
         left = 0
+        best_left = 0
+        best_length = float('inf')
+        formed = 0
 
         for right in range(len(s)):
-            counter_s[s[right]] += 1
+            right_ch = s[right]
+            counter_s[right_ch] += 1
 
-            while Counter(counter_s) & counter_t == counter_t:
-                if right - left + 1 < len(ans):
-                    ans = s[left:right+1]
-                counter_s[s[left]] -= 1
+            if right_ch in counter_t and counter_s[right_ch] == counter_t[right_ch]:
+                formed += 1
+
+            while formed == len(counter_t):
+                window_len = right - left + 1
+                
+                if window_len < best_length:
+                    best_left = left
+                    best_length = window_len
+                
+                left_ch = s[left]
+                counter_s[left_ch] -= 1
+
+                if left_ch in counter_t and counter_s[left_ch] < counter_t[left_ch]:
+                    formed -= 1
+
                 left += 1
         
-        return ans
+        if best_length == float('inf'):
+            return ""
+
+        return s[best_left:best_left + best_length]
 
             

20bc3e2ceb6fab2431bb36552be51b5a21e0c408
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-06 01:02:06 +0530

    Time: 0 ms (100%), Space: 20.5 MB (37.25%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,37 +1,16 @@
-import bisect
-
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
 
-        """
-        Pattern - Binary Search
+        while left <= right:
+            mid = left + (right - left) // 2
 
-        Time Complexity - O(log n)
-        Space Complexity - 0
-
-        """
-        
-        # l = 0
-        # r = len(nums)-1
-
-
-
-        # while l<=r:
-        #     mid = l + (r-l)//2
-
-        #     if nums[mid] == target:
-        #         return mid
-        #     elif nums[mid]<target:
-        #         l = mid+1
-        #     else:
-        #         r = mid-1
-        
-        # return -1
-
-        ind = bisect.bisect_left(nums, target)
-
-        if ind<len(nums) and nums[ind] == target:
-            return ind
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
         
         return -1
-

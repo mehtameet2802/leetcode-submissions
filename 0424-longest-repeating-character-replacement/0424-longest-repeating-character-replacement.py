cda149b44faf1d1e16d5f459d85552e15598cd6f
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-06 22:53:29 +0530

    Time: 83 ms (60.33%), Space: 19.6 MB (52.38%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,32 +1,38 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        highest_ele = s[0]
-        highest_freq = 0
-        arr = [0]*26
-        ans = 0
+        '''
+        left = 0
+        right will come from for loop
+        high_freq
+        high_ele
+        max_length
+        arr - of len 26, to maintain freq
+        every time in for loop a new ch comes, increment its freq
+        window_len = right - left + 1
+        if window_len - high_freq > k, start incrementing left, and each time decrement the freq of left ch
+        once done update max length
+        finally return max_length
+        '''
 
         left = 0
-        right = 0
+        arr = [0]*26
+        highest_freq = 0
+        highest_ele = ""
+        max_length = 0
 
-        while right < len(s):
-
+        for right in range(len(s)):
             arr[ord(s[right]) - ord('A')] += 1
-            if arr[ord(s[right]) - ord('A')] > highest_freq:
+
+            if highest_freq < arr[ord(s[right]) - ord('A')]:
                 highest_freq = arr[ord(s[right]) - ord('A')]
                 highest_ele = s[right]
 
-            window_len = right-left+1
-            
+            window_len = right - left + 1
             while window_len - highest_freq > k:
                 arr[ord(s[left]) - ord('A')] -= 1
-                window_len -=1
+                window_len -= 1
                 left += 1
-                
-            ans = max(ans,window_len)
-
-            right += 1
-
-        return ans
             
-
-
+            max_len = max(max_length, window_len)
+            
+        return max_len

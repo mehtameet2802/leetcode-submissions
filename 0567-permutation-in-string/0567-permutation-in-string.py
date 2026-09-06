f2a7f1a652a55200e0332f4df2b9e8e9eb9c2d3c
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-06 16:42:49 +0530

    Time: 19 ms (59.85%), Space: 19.1 MB (97.68%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,24 +1,20 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        counter_s1 = defaultdict(int)
-        counter_s2 = defaultdict(int)
+        counter_s1 = [0] * 26
+        counter_s2 = [0] * 26
 
         for ch in s1:
-            counter_s1[ch] += 1
+            counter_s1[ord(ch)-ord('a')] += 1
 
         k = len(s1)
 
         left = 0
 
         for right, ch in enumerate(s2):
-            counter_s2[ch] += 1
+            counter_s2[ord(ch)-ord('a')] += 1
 
             while (right - left + 1) > k:
-                counter_s2[s2[left]] -= 1
-
-                if counter_s2[s2[left]] == 0:
-                    del counter_s2[s2[left]]
-
+                counter_s2[ord(s2[left])-ord('a')] -= 1
                 left += 1
             
             if counter_s1 == counter_s2:

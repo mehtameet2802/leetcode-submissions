7e09bf1ead237c0b333e73f71b3e14dafa0b206e
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-06 15:26:21 +0530

    Time: 31 ms (65.06%), Space: 23.6 MB (53.01%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.py
@@ -1,40 +1,17 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        '''
-        DIrectly started coding and stuck in middle to actually find the splution, went to excalidraw and figured out the solution 
-        the tc is of O(n*p)
-        sc - O(p)
-        getting TLE
-
-        30 min spent
-
-        still no optimized solution found
-
-        Spent more 15 min and added a new while loop but now the problem is that hte negative numbers divisible by k are not being considered
-
-        Then referred gpt and got that if remainder of 2 numbers is same then their diff is divisble by the number used to calculate ites remainder
-
-        Time - 1 hr
-
-        Final TC - O(n)
-        Final SC - O(k)
-
-        '''
-
-
-        remainder_dict = defaultdict(int)
-        remainder_dict[0] = 1
+        remainders = defaultdict(int)
+        remainders[0] += 1
 
         prefix_sum = 0
+        ans = 0
 
-        subarray_cnt = 0
-
-        for cur_ele in nums:
-            prefix_sum += cur_ele
-
+        for num in nums:
+            prefix_sum += num
             remainder = prefix_sum % k
-            subarray_cnt += remainder_dict[remainder]
 
-            remainder_dict[remainder] += 1
-            
-        return subarray_cnt
+            ans += remainders[remainder]
+
+            remainders[remainder] += 1
+
+        return ans

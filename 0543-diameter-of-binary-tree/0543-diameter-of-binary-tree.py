7cab3fec24b13db83e7e163d90a40858cea0023d
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-08 16:18:57 +0530

    Time: 2 ms (80.44%), Space: 22.3 MB (36.14%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -6,11 +6,29 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        
-        diameter = 0
+        '''
+        Required output: - diameter
+        What helper(node) returns: max length of left or right, + 1
+        What final answer represents: dimaeter of the tree
+        Invariant: node != null
+        Pseudocode:
+        initiate ans = 0
+        caller helper for root node
+        inside the helper if node is null return 0
+        call helper for node.left
+        call helper for node.right
+        add result of left and right (ie depth of left and right nodes), compare it with global ans to get the diameter
+        compare return result of left and right, whichever depth is maximum add 1 and return it.
+        outside the helper definition return the ans
+
+        Complexity: O(n)
+        '''
+
+
+        ans = 0
 
         def helper(node):
-            nonlocal diameter 
+            nonlocal ans
 
             if not node:
                 return 0
@@ -18,9 +36,9 @@ class Solution:
             left = helper(node.left)
             right = helper(node.right)
 
-            diameter = max(diameter, left + right)
+            ans = max(ans, left + right)
 
-            return max(left, right) + 1
+            return max(left,right) + 1
         
         helper(root)
-        return diameter
+        return ans

2fb6cbab6434b4d961e91911681c198e41e220af
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-09 14:51:04 +0530

    Time: 0 ms (100%), Space: 19.6 MB (25.49%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.py
@@ -5,28 +5,42 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-        cnt = 1
+        '''
+        What node must stay connected before the reversed section?
+        What is the first node of the section before reversal? - before
+        What node follows the reversed section? - prev
+        What does each pointer represent?
+        prev - previous
+        before- before left
+        temp - used temperary reversal
+        Invariant during reversal: while head
+        How do I reconnect both ends? - 
+        Dangerous case: left = 1, we will add dummy node
+        Complexity: O(n)
+        '''
+
         dummy = ListNode()
         dummy.next = head
         prev = dummy
+        cnt = 1
 
         while cnt < left:
-            cnt += 1
             prev = head
             head = head.next
-            
+            cnt += 1
+
         before = prev
-        temp = head
 
         while cnt <= right:
+            temp = head
             head = head.next
             temp.next = prev
             prev = temp
             temp = head
             cnt += 1
         
-
         before.next.next = head
         before.next = prev
 
         return dummy.next
+

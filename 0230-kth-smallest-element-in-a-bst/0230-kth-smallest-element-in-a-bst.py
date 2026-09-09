71bb4554419ad9ca6cba744f7e4e2418c077546e
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-09 15:59:54 +0530

    Time: 3 ms (28.68%), Space: 22.4 MB (23.24%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,28 +6,37 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
-        ans = -1
+        '''
+        Required output: k th smallest element from all the values
+        Brute force: - append all the values in an array, sort the array, return kth element from right
+        Key state:
+        Invariant:
+        Dangerous case:
+
+        Pseudocode:
+        take a max_heap of size k
+        iterate through all the nodes and append value in max_heap
+        if size of max_heap >k then remove top node, until size of max_heap !=k
+        once all the nodes are iterated return -max_heap[0] as ans
+
+        Complexity: TC - O(n log n), SC - O(n)
+        '''
+
+        max_heap = []
 
         def helper(node):
-            nonlocal k, ans
-
             if not node:
-                return False
+                return
+                
+            heapq.heappush(max_heap, -node.val)
 
-            if helper(node.left):
-                return True
-
-            k -= 1
-            if k == 0:
-                ans = node.val
-                return True
-
-            if helper(node.right):
-                return True
+            while len(max_heap) > k:
+                heapq.heappop(max_heap)
             
-            return False
+            helper(node.left)
+            helper(node.right)
         
         helper(root)
-        return ans
+        return -max_heap[0]
+
 

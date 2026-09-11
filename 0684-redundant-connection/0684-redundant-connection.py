4fd5af7c941b3e3e35295c9ee1a8fbb922f1845e
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-11 18:52:58 +0530

    Time: 2 ms (66.53%), Space: 19.7 MB (27.84%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,42 +1,46 @@
 class DSU:
+
     def __init__(self,n):
-        self.parent = list(range(n))
-        self.size = [1]*n
-
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        
-        return self.parent[x]
+        self.parent = [idx for idx in range(n+1)] 
+        self.size = [1]*(n+1)
     
-    def union(self, a, b):
-        root_a = self.find(a)
-        root_b = self.find(b)
+    def find(self,node):
+        if self.parent[node] != node:
+            self.parent[node] = self.find(self.parent[node])
 
-        if root_a == root_b:
+        return self.parent[node]
+
+    def union(self, first, second):
+        first_parent = self.find(first)
+        second_parent = self.find(second)
+
+        if first_parent == second_parent:
             return False
+
+        if self.size[second_parent] < self.size[first_parent]:
+            first_parent, second_parent = second_parent, first_parent
         
-        if self.size[root_a] < self.size[root_b]:
-            root_a, root_b = root_b, root_a
-        
-        self.parent[root_b] = root_a
-        self.size[root_a] += self.size[root_b]
+        self.parent[second_parent] = first_parent
+        self.size[first_parent] += self.size[second_parent]
 
         return True
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        dsu = DSU(n+1)
+        '''
+        What makes an edge redundant?
+        What information must be known before accepting an edge?
+        How will connected components be represented?
+        How are two components combined?
+        What condition identifies the answer?
+        Invariant:
+        Complexity:
 
-        ans = []
+        I think we should DSU, as in this problem the edges are provided one after other and its asked to determine of adding this edge will lead to a cycle or not, a cycle occur if we cannot union as the parent of the nodes in new edge being added is already same, hence new edge cannot be added, and the very first edge that cannot be added by union is out answer
+        '''
 
-        for a,b in edges:
-            if not dsu.union(a,b):
-                return [a,b]
-        #         ans.append([a,b])
-        
-        # return ans[-1]
+        dsu = DSU(len(edges))
 
-
-        
+        for u,v in edges:
+            if not dsu.union(u,v):
+                return [u,v]

9a2ffc515bc93fb0085d0aa87a9a074156dd2bde
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-12 18:13:49 +0530

    Time: 0 ms (100%), Space: 19.4 MB (13.42%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,50 +1,63 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         '''
-        TC = O(R × C)
-        SC = O(R × C)
+        Required output:
+        What changes during one minute?
+        What information must one state contain?
+        Pattern prediction:
+        Why this pattern fits:
+        Initial state:
+        Transition rule:
+        When is a cell processed?
+        Termination condition:
+        Impossible-case condition:
+        Invariant:
+        Expected TC and SC:
         '''
+        dirs = [[0,1],[1,0],[-1,0],[0,-1]]
+        time = 0
+        good = 0
         ROWS = len(grid)
         COLS = len(grid[0])
-        dirs = [[0,1],[1,0],[-1,0],[0,-1]]
 
-        queue = deque([])
-        perfect = 0
-        empty = 0
+        queue = deque()
 
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == 2:
-                    queue.append((i,j))
-                elif grid[i][j] == 1:
-                    perfect += 1
-                else:
-                    empty += 1
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 2:
+                    queue.append((r,c))
+                elif grid[r][c] == 1:
+                    good += 1
+
+        if good == 0:
+            return time
         
-        if empty == ROWS*COLS:
-            return 0
-        
-        ans = 0
         while queue:
+
             length = len(queue)
 
             for _ in range(length):
+
                 r,c = queue.popleft()
 
                 for dr, dc in dirs:
                     nr = r + dr
                     nc = c + dc
 
-                    if nr<0 or nc<0 or nr>=ROWS or nc>=COLS or grid[nr][nc]!=1:
+                    if nr<0 or nc<0 or nr>=ROWS or nc>=COLS:
+                        continue
+                    
+                    if grid[nr][nc] != 1:
                         continue
                     
                     grid[nr][nc] = 2
-                    perfect -= 1
                     queue.append((nr,nc))
+                    good -= 1
             
-            ans += 1
+            time += 1
+        
 
-        if perfect > 0:
+        if good > 0:
             return -1
         
-        return ans-1
+        return time-1

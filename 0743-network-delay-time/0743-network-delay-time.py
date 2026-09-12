b4e7203afd7be0b9286b17b44383676db8b25ec7
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-12 15:11:01 +0530

    Time: 334 ms (69.79%), Space: 21.3 MB (39.68%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,30 +1,49 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        ans = [float('inf')] *n
-        ans[k-1] = 0
+        '''
+        Required output:
+        Graph direction: directed
+        Meaning of an edge: time to reach the other node
+        Are all edge costs equal? - no
+        What does the state for each node represent? 
+        What makes a candidate state better?
+        When can a node’s result be considered final?
+        Data structure needed:
+        InvariantInvariant invariant:
+        Danger update rule:
+        DangerExpected return value:
+        ExpectedExpected complexity3?
+        ExpectedExpected complexity:
+        ``Expected complexity:
+        Expected complexity:
+        '''
+
+        dist = [float('inf')]*(n+1)
 
         graph = defaultdict(list)
 
-        for a,b,w in times:
-            graph[a].append((b,w))
+        for u,v,w in times:
+            graph[u].append((v,w))
 
         min_heap = []
-        heapq.heappush(min_heap,(0,k))
+        heapq.heappush(min_heap, (0,k))
+        dist[k] = 0
 
         while min_heap:
-            dist, node = heapq.heappop(min_heap)
+            cur_dist, node = heapq.heappop(min_heap)
 
-            for nei, d in graph[node]:
-                if ans[nei-1] < dist:
-                    continue
-                
-                new_dist = d + dist
+            for nei, weight in graph[node]:
 
-                if ans[nei-1] > new_dist:
-                    ans[nei-1] = new_dist
-                    heapq.heappush(min_heap,(new_dist, nei))
-        
-        if float('inf') in ans:
-            return -1
-        
-        return max(ans)
+                nei_dist = cur_dist + weight
+
+                if nei_dist < dist[nei]:
+                    dist[nei] = nei_dist
+                    heapq.heappush(min_heap, (nei_dist, nei))
+
+        ans = -float('inf')
+        for node in range(1,n+1):
+            if dist[node] == float('inf'):
+                return -1
+            ans = max(ans,dist[node])
+
+        return ans 

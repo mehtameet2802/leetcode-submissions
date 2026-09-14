761262e4b6ba4e42af24eaeafc528ea93ca38c18
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-14 23:42:36 +0530

    Time: 52 ms (96.81%), Space: 31.8 MB (19.04%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -1,25 +1,6 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        # ans = float('inf')
-
-        # def helper(val,left,right,opt):
-        #     nonlocal ans
-
-        #     if val == 0:
-        #         ans = min(ans,opt)
-        #         return
-            
-        #     if val < 0 or left > right:
-        #         return
-
-        #     helper(val-nums[left], left+1, right, opt+1)
-        #     helper(val-nums[right], left, right-1, opt+1)
-        
-        # helper(x,0,len(nums)-1,0)
-        # return -1 if ans == float('inf') else ans
-
         total = sum(nums)
-
         target = total - x
 
         if target < 0:
@@ -40,11 +21,9 @@ class Solution:
                 left += 1
             
             if window_sum == target:
-                length = max(length, right-left+1)
-            
-        
+                length = max(length,right-left+1)
+
         if length == -1:
             return -1
         
         return len(nums) - length
-

21f95a80ad8bcd5980fb9d8e650a2188694f7d1c
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-16 14:50:27 +0530

    Time: 416 ms (14.68%), Space: 38 MB (56.79%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -1,36 +1,65 @@
 class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
         
-        distance = [[float('inf') for _ in range(len(edges))] for _ in range(2)]
-        graph = defaultdict(list)
+        # distance = [[float('inf') for _ in range(len(edges))] for _ in range(2)]
+        # graph = defaultdict(list)
 
-        for u,v in enumerate(edges):
-            if v == -1:
-                continue
-            graph[u].append(v)
+        # for u,v in enumerate(edges):
+        #     if v == -1:
+        #         continue
+        #     graph[u].append(v)
         
-        def djikstar(node_idx, node):
-            distance[node_idx][node] = 0
+        # def djikstar(node_idx, node):
+        #     distance[node_idx][node] = 0
 
-            min_heap = []
-            heapq.heappush(min_heap, (0,node))
+        #     min_heap = []
+        #     heapq.heappush(min_heap, (0,node))
 
-            while min_heap:
-                dist, cur_node = heapq.heappop(min_heap)
+        #     while min_heap:
+        #         dist, cur_node = heapq.heappop(min_heap)
 
-                if distance[node_idx][cur_node] < dist:
-                    continue
+        #         if distance[node_idx][cur_node] < dist:
+        #             continue
                 
-                for nei in graph[cur_node]:
-                    new_dist = dist + 1
+        #         for nei in graph[cur_node]:
+        #             new_dist = dist + 1
 
-                    if new_dist < distance[node_idx][nei]:
-                        distance[node_idx][nei] = new_dist
-                        heapq.heappush(min_heap, (new_dist,nei))
+        #             if new_dist < distance[node_idx][nei]:
+        #                 distance[node_idx][nei] = new_dist
+        #                 heapq.heappush(min_heap, (new_dist,nei))
             
         
-        djikstar(0, node1)
-        djikstar(1, node2)
+        # djikstar(0, node1)
+        # djikstar(1, node2)
+        # ans = float('inf')
+        # min_idx = 0
+
+        # for i in range(len(edges)):
+        #     if distance[0][i] == float('inf') or distance[1][i] == float('inf'):
+        #         continue
+
+        #     cur_max_dist = max(distance[0][i], distance[1][i])
+        #     if ans > cur_max_dist:
+        #         ans = cur_max_dist
+        #         min_idx = i
+
+        # return -1 if ans == float('inf') else min_idx
+
+
+        distance = [[float('inf') for _ in range(len(edges))] for _ in range(2)]
+        
+        def get_dist(node_idx, node):
+            cur_node = node
+            dist = 0
+
+            while cur_node != -1 and distance[node_idx][cur_node] == float('inf'):
+                distance[node_idx][cur_node] = dist
+                cur_node = edges[cur_node]
+                dist += 1
+            
+        
+        get_dist(0, node1)
+        get_dist(1, node2)
         ans = float('inf')
         min_idx = 0
 

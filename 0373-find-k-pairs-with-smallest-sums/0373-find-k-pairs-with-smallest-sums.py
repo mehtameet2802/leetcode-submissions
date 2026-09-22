4d97bc68e0504c48aac27c2ee3112a12fe6caf1e
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-23 00:30:35 +0530

    Time: 170 ms (11.63%), Space: 51.1 MB (8.77%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -1,30 +1,22 @@
 class Solution:
-    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        '''
-        m = len(num1)
-        SC - O(m)
-        TC - O(mlogm + klogm)
-        '''
-
-
+    def kSmallestPairs(self, nums1: list[int], nums2: list[int], k: int) -> list[list[int]]:
         min_heap = []
 
-        for idx,num1 in enumerate(nums1):
-            heapq.heappush(min_heap,(num1+nums2[0],idx,0))
+
+
+        for i, num in enumerate(nums1):
+            heapq.heappush(min_heap,(nums1[i]+nums2[0],i,0))
         
         ans = []
 
-        while len(ans)!=k:
-            ele_sum, num1_idx, num2_idx = heapq.heappop(min_heap)
+        while k>0:
+            sum_val, num1_idx, num2_idx = heapq.heappop(min_heap)
+            ans.append([nums1[num1_idx], nums2[num2_idx]])
 
-            num1 = nums1[num1_idx]
-            num2 = nums2[num2_idx]
-
-            ans.append([nums1[num1_idx],nums2[num2_idx]])
-
-            num2_idx += 1
-
-            if num2_idx < len(nums2):
-                heapq.heappush(min_heap, (num1+nums2[num2_idx],num1_idx,num2_idx))
+            new_num2_idx = num2_idx + 1
+            if new_num2_idx < len(nums2):
+                heapq.heappush(min_heap, (nums1[num1_idx]+nums2[new_num2_idx], num1_idx, new_num2_idx))
+            
+            k-=1
         
         return ans

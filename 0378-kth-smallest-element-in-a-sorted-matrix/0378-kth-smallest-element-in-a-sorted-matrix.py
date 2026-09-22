da013bc870b5437a60ac7c16c8cf022459c8b3e2
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-23 00:10:28 +0530

    Time: 31 ms (13%), Space: 23 MB (44.54%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -1,37 +1,20 @@
 class Solution:
-    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+    def kthSmallest(self, matrix: list[list[int]], k: int) -> int:
+        r = len(matrix)
 
-        '''
-        Pattern:
-        - K-Way Merge
-        - Min Heap
-        - Treat each row as a sorted list
-        - Keep one candidate from each row
-
-        TC:
-        O(k log n)
-
-        There are n rows, so the heap has at most n elements.
-        Each pop/push costs O(log n).
-        We perform k pops.
-
-        SC:
-        O(n)
-
-        The heap contains at most one element from
-        each of the n rows.
-        '''
         min_heap = []
 
-        for i in range(len(matrix)):
-            heapq.heappush(min_heap, (matrix[i][0], i, 0))
+        for i in range(r):
+            heapq.heappush(min_heap,(matrix[i][0],i,0))
         
-        while k>1:
-            val, row, ele = heapq.heappop(min_heap)
+        cnt = 0
 
-            if ele + 1 < len(matrix[row]):
-                heapq.heappush(min_heap, (matrix[row][ele+1],row,ele+1))
+        while min_heap and cnt < k-1:
+            ele, row_idx, ele_idx = heapq.heappop(min_heap)
+
+            if ele_idx+1 < len(matrix[0]):
+                heapq.heappush(min_heap, (matrix[row_idx][ele_idx+1], row_idx, ele_idx+1))
             
-            k -= 1
-        
-        return heapq.heappop(min_heap)[0]
+            cnt += 1
+            
+        return min_heap[0][0]

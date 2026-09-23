83b44157ee929503bcd1434d767176feacafe1c3
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-23 15:13:03 +0530

    Time: 0 ms (100%), Space: 20.9 MB (75.59%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -5,31 +5,18 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        '''
-        What does helper/node state represent? - the helper function when called on any node tells us if the node is valid or not, if valid then bydefault all its child nodes and child sub trees also become valid and vice versa
-
-        What condition makes a node valid? - the left subtree of the node must be valid and the right subtree of the node must be valid, along with the curretn node being valid
-        validity conditions - 
-        The left subtree of a node contains only nodes with keys strictly less than the node's key.The right subtree of a node contains only nodes with keys strictly greater than the node's key.
-
-        What must be true for every node in its left and right subtree? - all the values of the nodes in left subtree must be less than node's value and all the values for nodes in right subtree must be greater than node's value.
-
-        also if propogation is happening from the root node then we will have to pass limits such that both left and right nodes values are within their respective limits
-
-        TC - o(n)
-        sc - O(h), h is n or log n
-
-        '''
-
-        def helper(node,left,right):
+    def isValidBST(self, root: TreeNode | None) -> bool:
+        
+        def helper(node, left, right):
             if not node:
                 return True
+
+            if left >= node.val or right <= node.val:
+                return False
             
-            if left < node.val < right:
-                return helper(node.left,left,node.val) and helper(node.right,node.val,right)
+            left = helper(node.left, left, node.val)
+            right = helper(node.right, node.val, right)
 
-            return False
-
-        inf = float('inf')
-        return helper(root, -inf, inf)
+            return left and right and True
+        
+        return helper(root, -float('inf'), float('inf'))

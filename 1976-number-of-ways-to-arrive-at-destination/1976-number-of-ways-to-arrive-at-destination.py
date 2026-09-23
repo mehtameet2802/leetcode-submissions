f94ea24c3084a8447ce6de8418136b783b4ab3b3
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-23 20:22:15 +0530

    Time: 14 ms (76.1%), Space: 25.2 MB (73.12%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
@@ -1,32 +1,38 @@
 class Solution:
-    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+    def countPaths(self, n: int, roads: list[list[int]]) -> int:
+
         graph = defaultdict(list)
 
         for u,v,w in roads:
             graph[u].append((v,w))
             graph[v].append((u,w))
         
-        ans = [(float('inf'),0)] * n
-        ans[0] = (0,1)
-        min_heap = []
+        time = [float('inf')]*n
+        cnt = [0]*n
 
-        heapq.heappush(min_heap, (0,0))
+        time[0] = 0
+        cnt[0] = 1
+        min_heap = []
+        heapq.heappush(min_heap,(0,0))
 
         while min_heap:
-            t, node = heapq.heappop(min_heap)
+            cur_time, node = heapq.heappop(min_heap)
 
-            if t > ans[node][0]:
+            if cur_time > time[node]:
                 continue
+            
+            for nei,w in graph[node]:
+                new_time = cur_time + w
 
-            for nei, time in graph[node]:
-                new_time = time + t
+                if new_time < time[nei]:
+                    cnt[nei] = cnt[node]
+                    time[nei] = new_time
+                    heapq.heappush(min_heap,(new_time, nei))
+                elif new_time == time[nei]:
+                    cnt[nei] += cnt[node]
+        
+        return cnt[n-1] % (pow(10,9)+7)
+        
 
-                if new_time == ans[nei][0]:
-                    ans[nei] = (ans[nei][0], (ans[node][1]+ans[nei][1])%(pow(10,9)+7))
-                    continue
-                
-                elif new_time < ans[nei][0]:
-                    ans[nei] = (new_time,ans[node][1])
-                    heapq.heappush(min_heap, (new_time, nei))
 
-        return ans[n-1][1]
+

96bb66b3218adbac3fca289ef3314708e3db8d2c
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-23 21:06:45 +0530

    Time: 38 ms (51.22%), Space: 32.8 MB (69.01%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,32 +1,32 @@
 class DSU:
-    def __init__(self,n):
-        self.parent = list(range(n))
-        self.size = [1]*n
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.size = [1] * n
 
-    def find(self, x):
+    def find(self,x):
+
         if self.parent[x] != x:
             self.parent[x] = self.find(self.parent[x])
         
         return self.parent[x]
-    
-    def union(self, a, b):
-        root_a = self.find(a)
-        root_b = self.find(b)
 
-        if root_a == root_b:
+    def union(self, x,y):
+        first = self.find(x)
+        second = self.find(y)
+
+        if first == second:
             return False
         
-        if self.size[root_a] < self.size[root_b]:
-            root_a, root_b = root_b, root_a
+        if self.size[second] > self.size[first]:
+            first, second = second, first
         
-        self.parent[root_b] = root_a
-        self.size[root_a] += self.size[root_b]
+        self.parent[second] = first
+        self.size[first] += self.size[second]
 
         return True
 
-
 class Solution:
-    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+    def makeConnected(self, n: int, connections: list[list[int]]) -> int:
         
         if len(connections) < n-1:
             return -1
@@ -34,8 +34,9 @@ class Solution:
         dsu = DSU(n)
 
         components = n
-        for a,b in connections:
-            if dsu.union(a,b):
+
+        for u,v in connections:
+            if dsu.union(u,v):
                 components -= 1
-            
+        
         return components - 1

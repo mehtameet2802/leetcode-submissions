a0d734be6772e0e3708c931b7699753c9e9442cf
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-23 15:37:25 +0530

    Time: 0 ms (100%), Space: 19.5 MB (6.58%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,89 +1,16 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        # '''
-        # Pattern - Recursion + DP
+    def rob(self, nums: list[int]) -> int:
+        n = len(nums)
 
-        # TC - O(n)
-        # SC - O(n)
-        # '''
-
-        # mem = {}
-
-        # def helper(i):
-        #     if i in mem:
-        #         return mem[i]
-
-        #     if i>=len(nums):
-        #         return 0
-            
-        #     ans = max(helper(i+2)+nums[i],helper(i+1))
-        #     mem[i] = ans
-        #     return ans
-        
-        # return helper(0)
-
-
-        # '''
-        # Pattern - DP
-
-        # TC - O(n)
-        # SC - O(1)
-        # '''
-
-        # n = len(nums)
-        # if n<3:
-        #     return max(nums)
-        
-        # last = nums[n-1]
-        # prev1 = max(nums[n-1],nums[n-2])
-
-        # for i in range(n-3,-1,-1):
-        #     cur = max(nums[i]+last,prev1)
-        #     last, prev1 = prev1, cur
-        
-        # return prev1
-
-
-
-
-
-
-
-
-
-
-        if len(nums) < 2:
+        if n < 3:
             return max(nums)
 
-        n = len(nums)
-        dp = [0]*len(nums)
-        dp[n-1] = nums[n-1]
-        dp[n-2] = max(nums[n-2],nums[n-1])
+        first = nums[-1]
+        second = max(nums[-1],nums[-2])
 
         for i in range(n-3,-1,-1):
-            dp[i] = max(dp[i+1],dp[i+2]+nums[i])
+            cur = max(second, nums[i] + first)
+            first = second
+            second = cur
         
-        return max(dp[0], dp[1])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return second

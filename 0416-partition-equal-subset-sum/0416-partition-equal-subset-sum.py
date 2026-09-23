177de8367fa051cce85c862e0b718625abd6c7a4
Meet Mehta <80092236+mehtameet2802@users.noreply.github.com>
2026-09-23 17:41:46 +0530

    Time: 997 ms (39.83%), Space: 19.3 MB (84.63%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,49 +1,17 @@
 class Solution:
-    def canPartition(self, nums: List[int]) -> bool:
-        # total = sum(nums)
-
-        # if total % 2 :
-        #     return False
-
-        # target = total // 2
-        # dp = {}
-
-        # def helper(i,target):
-        #     if target == 0:
-        #         return True
-            
-        #     if target<0:
-        #         return False
-            
-        #     if i>=len(nums):
-        #         return False
-
-        #     if (i,target) in dp:
-        #         return dp[(i,target)]
-            
-        #     ans = helper(i+1, target - nums[i]) or helper(i+1, target)
-        #     dp[(i,target)] = ans
-        #     return ans
-            
-        
-        # return helper(0, target)
-
-
+    def canPartition(self, nums: list[int]) -> bool:
         total = sum(nums)
-
-        if total % 2 :
+        if total % 2 != 0:
             return False
-
+        
         target = total // 2
-        dp = [False] * (target + 1)
+
+        dp = [False]*(target + 1)
         dp[0] = True
 
         for num in nums:
-
-            for t in range(target, num-1, -1):
-                dp[t] = dp[t] or dp[t-num]
-            
+            for cur in range(target,0,-1):
+                if cur - num >= 0:
+                    dp[cur] = dp[cur] or dp[cur-num]
+        
         return dp[target]
-            
-        
-        
